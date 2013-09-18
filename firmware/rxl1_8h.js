89bc0cfe6c8205fd23fe2084c9e6fc1bc41a0776
OpenWSN Builder <builder@openwsn.org>
2013-09-18 04:42:38 -0700

    removing old firmware documentation

--- firmware/rxl1_8h.js
+++
@@ -1,4 +0,0 @@
-var rxl1_8h =
-[
-    [ "rxl1_init", "rxl1_8h.html#ga4a004d71ee3106152559c6bd214ec97f", null ]
-];
--- firmware/structcc1101___f_s_t_e_s_t__reg__t.js
+++
@@ -1,4 +0,0 @@
-var structcc1101___f_s_t_e_s_t__reg__t =
-[
-    [ "FSTEST", "structcc1101___f_s_t_e_s_t__reg__t.html#aef06b1e4ec9f668a7adf588cef95a6de", null ]
-];
