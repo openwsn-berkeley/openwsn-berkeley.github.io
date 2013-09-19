1f01206b8b97569c60f1eb50616c4820da3ef7e9
OpenWSN Builder <builder@openwsn.org>
2013-09-19 04:43:16 -0700

    removing old firmware documentation

--- firmware/dir_6de029121fd771a5c4cc7c9109dacf65.js
+++
@@ -1,5 +0,0 @@
-var dir_6de029121fd771a5c4cc7c9109dacf65 =
-[
-    [ "cr_startup_lpc176x.c", "00std__eui64b_2src_2cr__startup__lpc176x_8c.html", "00std__eui64b_2src_2cr__startup__lpc176x_8c" ],
-    [ "main1.c", "main1_8c.html", "main1_8c" ]
-];
--- firmware/dir_a4942e8fa32ed0b0a7ea5cefa9f01bfd.js
+++
@@ -1,4 +0,0 @@
-var dir_a4942e8fa32ed0b0a7ea5cefa9f01bfd =
-[
-    [ "00std_eui64.c", "wsn430v13b_200std__eui64_200std__eui64_8c.html", "wsn430v13b_200std__eui64_200std__eui64_8c" ]
-];
