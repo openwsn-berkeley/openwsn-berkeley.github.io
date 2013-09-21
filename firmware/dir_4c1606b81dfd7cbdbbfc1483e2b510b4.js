f86eae9e49eb948482cd67b5ea1ca8ba0e781838
OpenWSN Builder <builder@openwsn.org>
2013-09-21 04:43:18 -0700

    adding new firmware documentation

---
+++ firmware/dir_4c1606b81dfd7cbdbbfc1483e2b510b4.js
@@ -0,0 +1,6 @@
+var dir_4c1606b81dfd7cbdbbfc1483e2b510b4 =
+[
+    [ "common.h", "common_8h.html", "common_8h" ],
+    [ "startup.c", "startup_8c.html", "startup_8c" ],
+    [ "startup.h", "startup_8h.html", "startup_8h" ]
+];
---
+++ firmware/structgyro__vars__t.js
@@ -0,0 +1,10 @@
+var structgyro__vars__t =
+[
+    [ "configured", "structgyro__vars__t.html#a4b7fa24ce455946d1aa2f0cc32938f3b", null ],
+    [ "reg_DLPF_FS", "structgyro__vars__t.html#a3cc2ae4ed8ca3f99a578f8bf03dbf2a1", null ],
+    [ "reg_INT_CFG", "structgyro__vars__t.html#ad0103774127fc9ae33512beec8729647", null ],
+    [ "reg_INT_STATUS", "structgyro__vars__t.html#a921e9c953d12412677f1e14726d525ed", null ],
+    [ "reg_PWR_MGM", "structgyro__vars__t.html#acd186706dbf9a8caa4ee6f5c79c2fc75", null ],
+    [ "reg_SMPLRT_DIV", "structgyro__vars__t.html#a256b1d6b0102a37cdcdf0b5102115605", null ],
+    [ "reg_WHO_AM_I", "structgyro__vars__t.html#a21367e27378421808a8763ab4ed75d7d", null ]
+];
