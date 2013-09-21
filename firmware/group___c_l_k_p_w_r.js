98a4415b5274ae8cc793179f74f6cd86782e8fc0
OpenWSN Builder <builder@openwsn.org>
2013-09-21 04:42:45 -0700

    removing old firmware documentation

--- firmware/group___c_l_k_p_w_r.js
+++
@@ -1,6 +0,0 @@
-var group___c_l_k_p_w_r =
-[
-    [ "CLKPWR Public Macros", "group___c_l_k_p_w_r___public___macros.html", "group___c_l_k_p_w_r___public___macros" ],
-    [ "CLKPWR Private Macros", "group___c_l_k_p_w_r___private___macros.html", "group___c_l_k_p_w_r___private___macros" ],
-    [ "CLKPWR Public Functions", "group___c_l_k_p_w_r___public___functions.html", "group___c_l_k_p_w_r___public___functions" ]
-];
--- firmware/group__imu.js
+++
@@ -1,8 +0,0 @@
-var group__imu =
-[
-    [ "imu_debugPrint", "group__imu.html#ga9467dac3ddeabe1cea044e5064a405c0", null ],
-    [ "imu_init", "group__imu.html#gaee92580b45484a763ef6f507ed59e31a", null ],
-    [ "imu_receive", "group__imu.html#ga4c059ec6a973818cc9fec567243a479a", null ],
-    [ "imu_sendDone", "group__imu.html#ga649d143ef86aedba318bf5a73eb62466", null ],
-    [ "imu_trigger", "group__imu.html#gae8774820e30a027d9a2f1ac0042f7d35", null ]
-];
