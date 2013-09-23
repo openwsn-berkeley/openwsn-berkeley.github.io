2fc66d43051c9378b8973fff40e64b9f7c57ac02
OpenWSN Builder <builder@openwsn.org>
2013-09-23 04:42:55 -0700

    removing old firmware documentation

--- firmware/structopensim__repl__radiotimer__get_period__t.js
+++
@@ -1,4 +0,0 @@
-var structopensim__repl__radiotimer__get_period__t =
-[
-    [ "period", "structopensim__repl__radiotimer__get_period__t.html#a258408d6d5d13a24bfa5211d81ce1682", null ]
-];
--- firmware/tcp__port__win_8c.js
+++
@@ -1,4 +0,0 @@
-var tcp__port__win_8c =
-[
-    [ "tcp_port_connect", "tcp__port__win_8c.html#a150a0d6fc1232f8710b542845195ba9c", null ]
-];
