a4fe91dfef42a2a060236582d7d67a8807ac60df
OpenWSN Builder <builder@openwsn.org>
2013-09-23 04:43:30 -0700

    adding new firmware documentation

---
+++ firmware/openmotestm_2eui64_8c.js
@@ -0,0 +1,6 @@
+var openmotestm_2eui64_8c =
+[
+    [ "ADDRESSOFFSET", "openmotestm_2eui64_8c.html#a50c56fcb152c6a4784a9c0aa8b0184eb", null ],
+    [ "eui64_get", "openmotestm_2eui64_8c.html#ae49dc9068e3f46bfd188579192fc036b", null ],
+    [ "eui64", "openmotestm_2eui64_8c.html#a574bc0e8cd65bb0a8c6d40826901ba4e", null ]
+];
---
+++ firmware/structcc2420___t_o_p_t_s_t__reg__t.js
@@ -0,0 +1,9 @@
+var structcc2420___t_o_p_t_s_t__reg__t =
+[
+    [ "ATESTMOD_MODE", "structcc2420___t_o_p_t_s_t__reg__t.html#a274d63246fbb6d48d1f72799a408df18", null ],
+    [ "ATESTMOD_PD", "structcc2420___t_o_p_t_s_t__reg__t.html#a22c84c6025959aead54db3bd8c3639e1", null ],
+    [ "RAM_BIST_RUN", "structcc2420___t_o_p_t_s_t__reg__t.html#a4d8e5934121813dbab11a67555a6de33", null ],
+    [ "reserved_w0", "structcc2420___t_o_p_t_s_t__reg__t.html#a47ca700c705c8add6d792cfa64045bd7", null ],
+    [ "TEST_BATTMON_EN", "structcc2420___t_o_p_t_s_t__reg__t.html#a31e078c482bc59822b0a3a62e55bc758", null ],
+    [ "VC_IN_TEST_EN", "structcc2420___t_o_p_t_s_t__reg__t.html#a9fcc1a5623962b401c30b7a1e52ba182", null ]
+];
