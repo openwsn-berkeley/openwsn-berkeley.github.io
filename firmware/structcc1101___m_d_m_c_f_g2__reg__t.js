7e2904a1ce0b58155b32d6773457b56e4e1cd62f
OpenWSN Builder <builder@openwsn.org>
2013-09-23 21:00:20 -0700

    removing old firmware documentation

--- firmware/structcc1101___m_d_m_c_f_g2__reg__t.js
+++
@@ -1,7 +0,0 @@
-var structcc1101___m_d_m_c_f_g2__reg__t =
-[
-    [ "DEM_DCFILT_OFF", "structcc1101___m_d_m_c_f_g2__reg__t.html#a58d96fe1c9fed13d654b22bc049ff169", null ],
-    [ "MANCHESTER_EN", "structcc1101___m_d_m_c_f_g2__reg__t.html#ae99673a599b53839d00af1e8f7bd640d", null ],
-    [ "MOD_FORMAT", "structcc1101___m_d_m_c_f_g2__reg__t.html#ad7e55a9fb57160783b8a27833793140c", null ],
-    [ "SYNC_MODE", "structcc1101___m_d_m_c_f_g2__reg__t.html#ac9ec52c470ca399f110ca1e62e03698b", null ]
-];
--- firmware/structschedule__vars__t.js
+++
@@ -1,9 +0,0 @@
-var structschedule__vars__t =
-[
-    [ "backoff", "structschedule__vars__t.html#a8d745544f7ef05d1c33ddf538c20f326", null ],
-    [ "backoffExponent", "structschedule__vars__t.html#a492b6e376523ff79a2d6aeaff5bc2d38", null ],
-    [ "currentScheduleEntry", "structschedule__vars__t.html#a79a2bc38fe7d7754896ce53f8ad2a900", null ],
-    [ "debugPrintRow", "structschedule__vars__t.html#aeca0d2f36ef66e0f939106f7a2d37f83", null ],
-    [ "frameLength", "structschedule__vars__t.html#a913c707afe608546cd7af8e0670795fb", null ],
-    [ "scheduleBuf", "structschedule__vars__t.html#a30f51f31243d2d50890c66ced9c94367", null ]
-];
