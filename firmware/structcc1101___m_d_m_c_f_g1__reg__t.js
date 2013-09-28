4816fa84f51cba1e9c0784730266a499c6ccba19
OpenWSN Builder <builder@openwsn.org>
2013-09-28 13:39:28 -0700

    removing old firmware documentation

--- firmware/structcc1101___m_d_m_c_f_g1__reg__t.js
+++
@@ -1,7 +0,0 @@
-var structcc1101___m_d_m_c_f_g1__reg__t =
-[
-    [ "CHANSPC_E", "structcc1101___m_d_m_c_f_g1__reg__t.html#a34aa03d3071e914b71c2613233c990a8", null ],
-    [ "FEC_EN", "structcc1101___m_d_m_c_f_g1__reg__t.html#abc29bd232b96054220261de96ffdab08", null ],
-    [ "NUM_PREAMBLE", "structcc1101___m_d_m_c_f_g1__reg__t.html#a54ab63a5caff4af234e8fd51f7415cb4", null ],
-    [ "unused_r0", "structcc1101___m_d_m_c_f_g1__reg__t.html#a31c7762c93075841b4de3a4fc2d5ef81", null ]
-];
--- firmware/test__i2c__connnected__slaves_8c.js
+++
@@ -1,5 +0,0 @@
-var test__i2c__connnected__slaves_8c =
-[
-    [ "main", "test__i2c__connnected__slaves_8c.html#a6288eba0f8e8ad3ab1544ad731eb7667", null ],
-    [ "connected", "test__i2c__connnected__slaves_8c.html#a483474f9df864e9a0f55efff5c899b51", null ]
-];
