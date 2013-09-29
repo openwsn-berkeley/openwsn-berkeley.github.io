ac8329722a2656b1625186afe07731a4dcd67a21
OpenWSN Builder <builder@openwsn.org>
2013-09-29 04:43:09 -0700

    removing old firmware documentation

--- firmware/structopensim__client__vars__t.js
+++
@@ -1,6 +0,0 @@
-var structopensim__client__vars__t =
-[
-    [ "conn_socket", "structopensim__client__vars__t.html#ab5c3df1ab201325796ba262d34c7decd", null ],
-    [ "rxBuffer", "structopensim__client__vars__t.html#aa74eaa948433e95a06bfa408e518768a", null ],
-    [ "txBuffer", "structopensim__client__vars__t.html#a5dfced4907285fc807f83cf85227a4cb", null ]
-];
