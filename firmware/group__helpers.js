b39bb170cd9d12e4ccf5181924ad8eac4ae3ecee
OpenWSN Builder <builder@openwsn.org>
2013-10-03 04:46:05 -0700

    removing old firmware documentation

--- firmware/group__helpers.js
+++
@@ -1,6 +0,0 @@
-var group__helpers =
-[
-    [ "IEEE802154", "group___i_e_e_e802154.html", "group___i_e_e_e802154" ],
-    [ "Random", "group___random.html", "group___random" ],
-    [ "PacketFunctions", "group___packet_functions.html", "group___packet_functions" ]
-];
