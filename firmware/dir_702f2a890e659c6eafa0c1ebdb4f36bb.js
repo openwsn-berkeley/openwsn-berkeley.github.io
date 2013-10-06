9d091c1a63f77b491bb42b9aa04d05905834c4a2
OpenWSN Builder <builder@openwsn.org>
2013-10-06 04:44:30 -0700

    removing old firmware documentation

--- firmware/dir_702f2a890e659c6eafa0c1ebdb4f36bb.js
+++
@@ -1,4 +0,0 @@
-var dir_702f2a890e659c6eafa0c1ebdb4f36bb =
-[
-    [ "01bsp_radio.c", "01bsp__radio_8c.html", "01bsp__radio_8c" ]
-];
--- firmware/structcc1101___f_s_c_t_r_l0__reg__t.js
+++
@@ -1,4 +0,0 @@
-var structcc1101___f_s_c_t_r_l0__reg__t =
-[
-    [ "FREQOFF", "structcc1101___f_s_c_t_r_l0__reg__t.html#ade606d36dd733c062fa6835f11e7d6b7", null ]
-];
