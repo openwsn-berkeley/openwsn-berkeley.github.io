2f0daebb3f1c626531fb65f087a6dc96554e686e
OpenWSN Builder <builder@openwsn.org>
2013-10-17 04:43:26 -0700

    removing old firmware documentation

--- firmware/group___button.js
+++
@@ -1,4 +0,0 @@
-var group___button =
-[
-    [ "button_init", "group___button.html#ga681470fea7c7eaa67094df3e5567a8f7", null ]
-];
--- firmware/structcc1101___v_c_o___v_c___d_a_c__reg__t.js
+++
@@ -1,4 +0,0 @@
-var structcc1101___v_c_o___v_c___d_a_c__reg__t =
-[
-    [ "VCO_VC_DAC", "structcc1101___v_c_o___v_c___d_a_c__reg__t.html#a6ded08b67f69ead43bf9d65f3ab6dcc6", null ]
-];
