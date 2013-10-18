a56b775bf21ca1b38ca04253f34319b8b2894d59
OpenWSN Builder <builder@openwsn.org>
2013-10-18 04:44:04 -0700

    removing old firmware documentation

--- firmware/dir_6191e2332e47cfe7511efc26b90ec8f9.js
+++
@@ -1,4 +0,0 @@
-var dir_6191e2332e47cfe7511efc26b90ec8f9 =
-[
-    [ "01bsp_bothtimers.c", "01bsp__bothtimers_8c.html", "01bsp__bothtimers_8c" ]
-];
--- firmware/wsn430v13b_200std__uart_200std__uart_8c.js
+++
@@ -1,5 +0,0 @@
-var wsn430v13b_200std__uart_200std__uart_8c =
-[
-    [ "main", "wsn430v13b_200std__uart_200std__uart_8c.html#a6288eba0f8e8ad3ab1544ad731eb7667", null ],
-    [ "USART1RX_ISR", "wsn430v13b_200std__uart_200std__uart_8c.html#a0ed30aeff5af23f687556bb6cdcdd8e4", null ]
-];
