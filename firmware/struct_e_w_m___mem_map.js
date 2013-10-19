b6aa2ceab65dffcf49a13aa3d88f0f81cd4628c7
OpenWSN Builder <builder@openwsn.org>
2013-10-19 04:43:58 -0700

    removing old firmware documentation

--- firmware/struct_e_w_m___mem_map.js
+++
@@ -1,7 +0,0 @@
-var struct_e_w_m___mem_map =
-[
-    [ "CMPH", "struct_e_w_m___mem_map.html#adfc24deefa11f544340595620f2a21c2", null ],
-    [ "CMPL", "struct_e_w_m___mem_map.html#a2c5c2a5318ec082d7f7f05ec60cbc8c6", null ],
-    [ "CTRL", "struct_e_w_m___mem_map.html#a8d39f2f3ea6b8ef4e4684a3507e90609", null ],
-    [ "SERV", "struct_e_w_m___mem_map.html#a6704c0fc9ce34f82302daddc97e160be", null ]
-];
--- firmware/structcc1101___a_g_c_c_t_r_l2__reg__t.js
+++
@@ -1,6 +0,0 @@
-var structcc1101___a_g_c_c_t_r_l2__reg__t =
-[
-    [ "MAGN_TARGET", "structcc1101___a_g_c_c_t_r_l2__reg__t.html#a28b7a7b92474e08bb7da62dc3a09c3d3", null ],
-    [ "MAX_DVGA_GAIN", "structcc1101___a_g_c_c_t_r_l2__reg__t.html#a987e4ce557251a1ace5b3e8617d76605", null ],
-    [ "MAX_LNA_GAIN", "structcc1101___a_g_c_c_t_r_l2__reg__t.html#a4583f219ae2bc9673d7cf325c874fe8b", null ]
-];
