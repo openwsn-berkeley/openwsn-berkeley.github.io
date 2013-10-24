851e510bc559643316857f13bd6d5449b266320e
OpenWSN Builder <builder@openwsn.org>
2013-10-24 04:45:13 -0700

    removing old firmware documentation

--- firmware/openrandom_8h.js
+++
@@ -1,5 +0,0 @@
-var openrandom_8h =
-[
-    [ "openrandom_get16b", "openrandom_8h.html#gaafd337b51477d5eeb5ae0d6489bb69e6", null ],
-    [ "openrandom_init", "openrandom_8h.html#ga226a338ea5cd81f0b9e92584540125a5", null ]
-];
--- firmware/structcc1101___p_k_t_l_e_n__reg__t.js
+++
@@ -1,4 +0,0 @@
-var structcc1101___p_k_t_l_e_n__reg__t =
-[
-    [ "PACKET_LENGTH", "structcc1101___p_k_t_l_e_n__reg__t.html#a4aa9ff851108b438098ec68c9fcade00", null ]
-];
