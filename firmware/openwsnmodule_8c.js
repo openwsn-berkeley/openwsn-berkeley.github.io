aedec0acbbd8fab2633b74e8f8bae8cbef34f4d1
OpenWSN Builder <builder@openwsn.org>
2013-10-27 04:45:01 -0700

    removing old firmware documentation

--- firmware/openwsnmodule_8c.js
+++
@@ -1,5 +0,0 @@
-var openwsnmodule_8c =
-[
-    [ "PyMODINIT_FUNC", "openwsnmodule_8c.html#a75c7c5d977b65e67c0b0d80fc079004f", null ],
-    [ "initopenwsn_generic", "openwsnmodule_8c.html#a81b088294f967080ba907630dfcb51c0", null ]
-];
