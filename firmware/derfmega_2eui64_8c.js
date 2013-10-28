23df6cac985f467b8e22205ee628026cc3b042ac
OpenWSN Builder <builder@openwsn.org>
2013-10-28 04:44:44 -0700

    removing old firmware documentation

--- firmware/derfmega_2eui64_8c.js
+++
@@ -1,6 +0,0 @@
-var derfmega_2eui64_8c =
-[
-    [ "EEPROM_read", "derfmega_2eui64_8c.html#ae31189cacf958956eca9378a4af59293", null ],
-    [ "EEPROM_write", "derfmega_2eui64_8c.html#ae24ed8b3f650031940d6e9eaff18fa26", null ],
-    [ "eui64_get", "derfmega_2eui64_8c.html#ae49dc9068e3f46bfd188579192fc036b", null ]
-];
--- firmware/structmacpong__vars__t.js
+++
@@ -1,4 +0,0 @@
-var structmacpong__vars__t =
-[
-    [ "timerId", "structmacpong__vars__t.html#a1952925d51e375d4da096207f03a8777", null ]
-];
