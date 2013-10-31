5e32f71d04abb3cadc36541e892d450f2e38199d
OpenWSN Builder <builder@openwsn.org>
2013-10-31 04:45:01 -0700

    removing old firmware documentation

--- firmware/dir_f09e7ea01dbcfb39732689f1bd68bce1.js
+++
@@ -1,4 +0,0 @@
-var dir_f09e7ea01dbcfb39732689f1bd68bce1 =
-[
-    [ "00std_timers.c", "00std__timers_8c.html", "00std__timers_8c" ]
-];
--- firmware/k20_2bsp_2uart__config_8h.js
+++
@@ -1,4 +0,0 @@
-var k20_2bsp_2uart__config_8h =
-[
-    [ "BAUD_RATE", "k20_2bsp_2uart__config_8h.html#ad4455691936f92fdd6c37566fc58ba1f", null ]
-];
