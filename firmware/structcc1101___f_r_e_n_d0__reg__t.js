79cfcc8bc3b006aa2e2f32d637e96c743dc05c2d
OpenWSN Builder <builder@openwsn.org>
2013-11-01 04:45:07 -0700

    removing old firmware documentation

--- firmware/structcc1101___f_r_e_n_d0__reg__t.js
+++
@@ -1,7 +0,0 @@
-var structcc1101___f_r_e_n_d0__reg__t =
-[
-    [ "LODIV_BUF_CURRENT_TX", "structcc1101___f_r_e_n_d0__reg__t.html#a2d0a17561ff2d332f0ddcaed5bb9b9cd", null ],
-    [ "PA_POWER", "structcc1101___f_r_e_n_d0__reg__t.html#a05b026c507d447c10c73a23d4a398488", null ],
-    [ "unused_r0_1", "structcc1101___f_r_e_n_d0__reg__t.html#a74ff0e285735c9cececc1e1b1309302b", null ],
-    [ "unused_r0_2", "structcc1101___f_r_e_n_d0__reg__t.html#ad0b9071204e7fef20b37204cc3176d86", null ]
-];
--- firmware/wdog_8h.js
+++
@@ -1,5 +0,0 @@
-var wdog_8h =
-[
-    [ "wdog_disable", "wdog_8h.html#a0b1954623f258c06db648d57b4871392", null ],
-    [ "wdog_unlock", "wdog_8h.html#a95268ceb7d30bbd32289ae32035a734d", null ]
-];
