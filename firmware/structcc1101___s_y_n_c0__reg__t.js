2d503d41098283f88720ca626a8e79ad22d2b42f
OpenWSN Builder <builder@openwsn.org>
2013-11-02 04:45:21 -0700

    removing old firmware documentation

--- firmware/structcc1101___s_y_n_c0__reg__t.js
+++
@@ -1,4 +0,0 @@
-var structcc1101___s_y_n_c0__reg__t =
-[
-    [ "SYNC0", "structcc1101___s_y_n_c0__reg__t.html#a85c087440ffcdedc82fa7c840d76d88f", null ]
-];
--- firmware/xpressohack_2eui64_8c.js
+++
@@ -1,8 +0,0 @@
-var xpressohack_2eui64_8c =
-[
-    [ "EUI_SIZE_IN_BYTES", "xpressohack_2eui64_8c.html#a3d0cf2bbbf31f6e8615c90865518e14c", null ],
-    [ "OPENWSN_EUI_B0", "xpressohack_2eui64_8c.html#a82aa45606df8cfbe3f8dababcfbfda0e", null ],
-    [ "OPENWSN_EUI_B1", "xpressohack_2eui64_8c.html#a5234aaca86f256521a3d24b41bb2f122", null ],
-    [ "OPENWSN_EUI_B2", "xpressohack_2eui64_8c.html#a10a0ce3d59f6f4179e28130bf92fd438", null ],
-    [ "eui64_get", "xpressohack_2eui64_8c.html#ae49dc9068e3f46bfd188579192fc036b", null ]
-];
