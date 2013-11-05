41db12a250ad21c6662a3ccd52f883b9c60ed73b
OpenWSN Builder <builder@openwsn.org>
2013-11-05 04:45:21 -0800

    removing old firmware documentation

--- firmware/struct_s_s_p___d_a_t_a___s_e_t_u_p___type.js
+++
@@ -1,9 +0,0 @@
-var struct_s_s_p___d_a_t_a___s_e_t_u_p___type =
-[
-    [ "length", "struct_s_s_p___d_a_t_a___s_e_t_u_p___type.html#aebb70c2aab3407a9f05334c47131a43b", null ],
-    [ "rx_cnt", "struct_s_s_p___d_a_t_a___s_e_t_u_p___type.html#a3b1e23a496d25dd87f7b1f223fe0d60c", null ],
-    [ "rx_data", "struct_s_s_p___d_a_t_a___s_e_t_u_p___type.html#a145057319bd95286db0e0ad0e635f93f", null ],
-    [ "status", "struct_s_s_p___d_a_t_a___s_e_t_u_p___type.html#ade20423e91627f07e610924cb0081623", null ],
-    [ "tx_cnt", "struct_s_s_p___d_a_t_a___s_e_t_u_p___type.html#a0441520d3f5ed5b07bfb4889dd495b86", null ],
-    [ "tx_data", "struct_s_s_p___d_a_t_a___s_e_t_u_p___type.html#ac5b2b9db8286b9c85c956556cfa42b92", null ]
-];
--- firmware/structopensim__requ__radiotimer__schedule__t.js
+++
@@ -1,4 +0,0 @@
-var structopensim__requ__radiotimer__schedule__t =
-[
-    [ "offset", "structopensim__requ__radiotimer__schedule__t.html#ac681806181c80437cfab37335f62ff39", null ]
-];
