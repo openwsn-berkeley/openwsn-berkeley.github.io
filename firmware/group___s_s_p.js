d3293e59a2176317d54684abd08437c964b61a83
OpenWSN Builder <builder@openwsn.org>
2013-11-06 04:45:33 -0800

    removing old firmware documentation

--- firmware/group___s_s_p.js
+++
@@ -1,8 +0,0 @@
-var group___s_s_p =
-[
-    [ "SSP Public Macros", "group___s_s_p___public___macros.html", "group___s_s_p___public___macros" ],
-    [ "SSP Private Macros", "group___s_s_p___private___macros.html", "group___s_s_p___private___macros" ],
-    [ "SSP Public Types", "group___s_s_p___public___types.html", "group___s_s_p___public___types" ],
-    [ "SSP Public Functions", "group___s_s_p___public___functions.html", "group___s_s_p___public___functions" ],
-    [ "_SSP", "group___s_s_p.html#ga4bbcc6ac233526a75040c15029532d13", null ]
-];
--- firmware/struct_f_s_m_c___bank2___type_def.js
+++
@@ -1,9 +0,0 @@
-var struct_f_s_m_c___bank2___type_def =
-[
-    [ "ECCR2", "struct_f_s_m_c___bank2___type_def.html#a36c9141e49fb251df9ba37a5c8037ff0", null ],
-    [ "PATT2", "struct_f_s_m_c___bank2___type_def.html#a302756f6502c6b497fcbff55d190c178", null ],
-    [ "PCR2", "struct_f_s_m_c___bank2___type_def.html#a42af74daffde8b18d805bdb0b9099283", null ],
-    [ "PMEM2", "struct_f_s_m_c___bank2___type_def.html#a367d5eb12a9323f0c6fa26f94bfcf798", null ],
-    [ "RESERVED0", "struct_f_s_m_c___bank2___type_def.html#abc00ffe9172a10966afc9df973447779", null ],
-    [ "SR2", "struct_f_s_m_c___bank2___type_def.html#a24222734ec9cbe6f6849fc2745ad0e89", null ]
-];
