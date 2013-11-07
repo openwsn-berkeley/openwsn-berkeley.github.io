9b145cb8e9a0f6efadc43408238642720ac97756
OpenWSN Builder <builder@openwsn.org>
2013-11-07 04:45:38 -0800

    removing old firmware documentation

--- firmware/structopensim__repl__sync__is_on__t.js
+++
@@ -1,4 +0,0 @@
-var structopensim__repl__sync__is_on__t =
-[
-    [ "isOn", "structopensim__repl__sync__is_on__t.html#a520469e03558361f60512aaafd81ad37", null ]
-];
--- firmware/tower_8h.js
+++
@@ -1,4 +0,0 @@
-var tower_8h =
-[
-    [ "DEBUG_PRINT", "tower_8h.html#ad5aeca1a1a793c8ba18f85cbac89e768", null ]
-];
