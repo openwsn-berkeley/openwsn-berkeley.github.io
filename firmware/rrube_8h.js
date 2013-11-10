0623bda857a21e1454c65f4c303e8d9a29be6512
OpenWSN Builder <builder@openwsn.org>
2013-11-10 04:46:45 -0800

    adding new firmware documentation

---
+++ firmware/rrube_8h.js
@@ -0,0 +1,4 @@
+var rrube_8h =
+[
+    [ "rrube_init", "rrube_8h.html#gaf00d308c5405782a753cf53a5456064e", null ]
+];
---
+++ firmware/structrtc__timer__vars__t.js
@@ -0,0 +1,4 @@
+var structrtc__timer__vars__t =
+[
+    [ "alarm_cb", "structrtc__timer__vars__t.html#a53b27fd4895472e09dfaa21f7d9ed5d1", null ]
+];
