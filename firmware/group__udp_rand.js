7e018038e4ba5ae03769fae07297595ff2657d91
OpenWSN Builder <builder@openwsn.org>
2014-01-08 04:45:19 -0800

    removing old firmware documentation

--- firmware/group__udp_rand.js
+++
@@ -1,9 +0,0 @@
-var group__udp_rand =
-[
-    [ "udprand_debugPrint", "group__udp_rand.html#gad4d54912ca201c79729b63d1d69041f9", null ],
-    [ "udprand_init", "group__udp_rand.html#ga42f99b3c556f0a0f083a97decf96ff43", null ],
-    [ "udprand_receive", "group__udp_rand.html#gac7d4ca31ed3b088ba59ebd1cd58c1d94", null ],
-    [ "udprand_sendDone", "group__udp_rand.html#gaf1a0a6d3930b350883f953de08f001a4", null ],
-    [ "udprand_task", "group__udp_rand.html#ga3b13714c3b8c6692a04f3ef3714c13d9", null ],
-    [ "udprand_trigger", "group__udp_rand.html#ga1cd379bad86d80f18cb34ad0c43d8310", null ]
-];
