48339536c5c071a56bc99b087b8878b4870081d2
OpenWSN Builder <builder@openwsn.org>
2014-01-08 04:45:57 -0800

    adding new firmware documentation

---
+++ firmware/openal__event__manager_8h.js
@@ -0,0 +1,15 @@
+var openal__event__manager_8h =
+[
+    [ "OPENAL_EVENT_QUEUE_LEN", "openal__event__manager_8h.html#ab536ae836f93117d8c7de6fa4c8c8071", null ],
+    [ "openal_event_manager_init", "openal__event__manager_8h.html#a97ab0fecb9e2dfb16ec08ffd37ad3bd7", null ],
+    [ "adc_conversion_semaphore", "openal__event__manager_8h.html#a91ecade5ff6275837ea40e19bf834435", null ],
+    [ "event_listener_config_mutex", "openal__event__manager_8h.html#af2619105fbea888a15306983afcaf670", null ],
+    [ "openal_event_queues", "openal__event__manager_8h.html#a23610e6d5fef2bf506883d9c004f8a93", null ],
+    [ "openal_GPIO_Listener_type", "openal__event__manager_8h.html#a462458f1232c33e56495b8b6e7a4e058", null ],
+    [ "openal_GPIO_Listeners", "openal__event__manager_8h.html#aeb8a58b9954ff929313434ce31269e7c", null ],
+    [ "openal_SOCK_Listener", "openal__event__manager_8h.html#a2542ab69362e0b19b76238dbb2360e20", null ],
+    [ "openal_UARTA_Listener", "openal__event__manager_8h.html#a541caf6ef210420b59fd7e0bc4973e29", null ],
+    [ "openal_UARTB_Listener", "openal__event__manager_8h.html#a1ea8c3f01fe1a120dfbd5639df8b1b23", null ],
+    [ "openal_UARTC_Listener", "openal__event__manager_8h.html#a3dcfdaea16fef79aa5c13f805cb407f1", null ],
+    [ "openal_UDP_Listener", "openal__event__manager_8h.html#a058a991d38086e6fc2813cb99685e1a7", null ]
+];
---
+++ firmware/rcc_8c.js
@@ -0,0 +1,5 @@
+var rcc_8c =
+[
+    [ "RCC_Configuration", "rcc_8c.html#ab7945e1fc61902deac23a942a2e2d95d", null ],
+    [ "RCC_Wakeup", "rcc_8c.html#a7a67d58de8bb03614a8edb8e1ee6f102", null ]
+];
