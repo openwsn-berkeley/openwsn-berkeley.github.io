fd53d6cbf07025f6b251a4f8621c2e94ebd1ff02
OpenWSN Builder <builder@openwsn.org>
2014-01-11 04:45:16 -0800

    removing old firmware documentation

--- firmware/01bsp__leds_8c.js
+++
@@ -1,4 +0,0 @@
-var 01bsp__leds_8c =
-[
-    [ "mote_main", "01bsp__leds_8c.html#a250f6a5899d659eb4f937ca9ceef255b", null ]
-];
--- firmware/struct_u_s_a_r_t___init_type_def.js
+++
@@ -1,9 +0,0 @@
-var struct_u_s_a_r_t___init_type_def =
-[
-    [ "USART_BaudRate", "struct_u_s_a_r_t___init_type_def.html#ac205846b76d8a31f1320a93db8e0598d", null ],
-    [ "USART_HardwareFlowControl", "struct_u_s_a_r_t___init_type_def.html#af8b5a105166440a5c6c71dd8dfeb9560", null ],
-    [ "USART_Mode", "struct_u_s_a_r_t___init_type_def.html#a6c0bdaa6ff306aba8cc5b0d64db88f0f", null ],
-    [ "USART_Parity", "struct_u_s_a_r_t___init_type_def.html#a25b591b26592907581ddd1376a1d1f74", null ],
-    [ "USART_StopBits", "struct_u_s_a_r_t___init_type_def.html#a800f2b845a0c6948b0c6c91fbc3257d4", null ],
-    [ "USART_WordLength", "struct_u_s_a_r_t___init_type_def.html#ad216f57c3f4fa80b3c19420c4f791cad", null ]
-];
