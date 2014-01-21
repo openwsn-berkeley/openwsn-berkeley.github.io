7e33fee7f9ba2bba1b2ce54461a60e95985825fd
OpenWSN Builder <builder@openwsn.org>
2014-01-21 04:45:48 -0800

    adding new firmware documentation

---
+++ firmware/mcg_8c.js
@@ -0,0 +1,9 @@
+var mcg_8c =
+[
+    [ "clk_monitor_0", "mcg_8c.html#a374169258f9d683e898390fc77d27620", null ],
+    [ "fll_freq", "mcg_8c.html#a030685ed5ebce3eef983e9410e7a1584", null ],
+    [ "what_mcg_mode", "mcg_8c.html#a721d6c6108a90f104913f9c3dc01cac8", null ],
+    [ "core_clk_khz", "mcg_8c.html#a7fa854056a09162506d2e1e8ca57fd6d", null ],
+    [ "dmx32_val", "mcg_8c.html#a4364c6c8ebbce815962149280ea941cc", null ],
+    [ "drs_val", "mcg_8c.html#a66bc4b902ddebd3bd0f8cd71bc2f9441", null ]
+];
