61aadc9e9447fcee4518623517ff4dd263cc12ed
OpenWSN Builder <builder@openwsn.org>
2014-01-21 04:45:14 -0800

    removing old firmware documentation

--- firmware/structradio__icb__t.js
+++
@@ -1,5 +0,0 @@
-var structradio__icb__t =
-[
-    [ "endFrame_cb", "structradio__icb__t.html#aec819e3fb0e9ba93babb7ee9499b5052", null ],
-    [ "startFrame_cb", "structradio__icb__t.html#a7f4857055531fad52ec6da393d826076", null ]
-];
--- firmware/wsn430v13b_200std__i2c_200std__i2c_8c.js
+++
@@ -1,4 +0,0 @@
-var wsn430v13b_200std__i2c_200std__i2c_8c =
-[
-    [ "main", "wsn430v13b_200std__i2c_200std__i2c_8c.html#a6288eba0f8e8ad3ab1544ad731eb7667", null ]
-];
