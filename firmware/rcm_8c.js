9fde0cd79eb15bca0f5b952fe344b567c81ce684
OpenWSN Builder <builder@openwsn.org>
2014-01-27 22:02:40 -0800

    removing old firmware documentation

--- firmware/rcm_8c.js
+++
@@ -1,4 +0,0 @@
-var rcm_8c =
-[
-    [ "software_reset", "rcm_8c.html#a8585da6db7271a7c97ea5db64022765f", null ]
-];
--- firmware/start_8c.js
+++
@@ -1,6 +0,0 @@
-var start_8c =
-[
-    [ "cpu_identify", "start_8c.html#a39eb1cb8c56fb374b2c2bc63d75ee2cf", null ],
-    [ "flash_identify", "start_8c.html#a8c99e1e3718689f601ecdb259e63a0bb", null ],
-    [ "start", "start_8c.html#aedc96a40e844bc2efc8bbef1b0782702", null ]
-];
