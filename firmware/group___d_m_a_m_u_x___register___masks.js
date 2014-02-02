f66b178769abf41ea53355cea78b38ace0d0704a
OpenWSN Builder <builder@openwsn.org>
2014-02-02 04:46:38 -0800

    adding new firmware documentation

---
+++ firmware/group___d_m_a_m_u_x___register___masks.js
@@ -0,0 +1,17 @@
+var group___d_m_a_m_u_x___register___masks =
+[
+    [ "DMAMUX_CHCFG_ENBL_MASK", "group___d_m_a_m_u_x___register___masks.html#ga311ccb0a9a00f29da44f8c41b33ba79f", null ],
+    [ "DMAMUX_CHCFG_ENBL_MASK", "group___d_m_a_m_u_x___register___masks.html#ga311ccb0a9a00f29da44f8c41b33ba79f", null ],
+    [ "DMAMUX_CHCFG_ENBL_SHIFT", "group___d_m_a_m_u_x___register___masks.html#ga23d6f41370761b5c68e4d49f419aaee9", null ],
+    [ "DMAMUX_CHCFG_ENBL_SHIFT", "group___d_m_a_m_u_x___register___masks.html#ga23d6f41370761b5c68e4d49f419aaee9", null ],
+    [ "DMAMUX_CHCFG_SOURCE", "group___d_m_a_m_u_x___register___masks.html#ga26ed125b670223f11ea326335729bb9b", null ],
+    [ "DMAMUX_CHCFG_SOURCE", "group___d_m_a_m_u_x___register___masks.html#ga26ed125b670223f11ea326335729bb9b", null ],
+    [ "DMAMUX_CHCFG_SOURCE_MASK", "group___d_m_a_m_u_x___register___masks.html#ga35b279ba0b1c9e817901494cdac305c5", null ],
+    [ "DMAMUX_CHCFG_SOURCE_MASK", "group___d_m_a_m_u_x___register___masks.html#ga35b279ba0b1c9e817901494cdac305c5", null ],
+    [ "DMAMUX_CHCFG_SOURCE_SHIFT", "group___d_m_a_m_u_x___register___masks.html#gac2b7553c4599d8e919750598dd03f8a3", null ],
+    [ "DMAMUX_CHCFG_SOURCE_SHIFT", "group___d_m_a_m_u_x___register___masks.html#gac2b7553c4599d8e919750598dd03f8a3", null ],
+    [ "DMAMUX_CHCFG_TRIG_MASK", "group___d_m_a_m_u_x___register___masks.html#gafd2b6158f86bedffb640e73c40cdd0f5", null ],
+    [ "DMAMUX_CHCFG_TRIG_MASK", "group___d_m_a_m_u_x___register___masks.html#gafd2b6158f86bedffb640e73c40cdd0f5", null ],
+    [ "DMAMUX_CHCFG_TRIG_SHIFT", "group___d_m_a_m_u_x___register___masks.html#ga714a6b142fde49d701e3f624bb2417e1", null ],
+    [ "DMAMUX_CHCFG_TRIG_SHIFT", "group___d_m_a_m_u_x___register___masks.html#ga714a6b142fde49d701e3f624bb2417e1", null ]
+];
---
+++ firmware/struct_g_p_i_o___init_type_def.js
@@ -0,0 +1,6 @@
+var struct_g_p_i_o___init_type_def =
+[
+    [ "GPIO_Mode", "struct_g_p_i_o___init_type_def.html#a0c7e8901d8b511bbb8c3b153f705dbba", null ],
+    [ "GPIO_Pin", "struct_g_p_i_o___init_type_def.html#a982814b45f424a6a0927752ba07e89c8", null ],
+    [ "GPIO_Speed", "struct_g_p_i_o___init_type_def.html#a57b08335216f50618ebc080e4fbb0a80", null ]
+];
