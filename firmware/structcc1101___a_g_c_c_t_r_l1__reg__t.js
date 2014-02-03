91efc689c590368e5b606f287ca39095c9e53fcf
OpenWSN Builder <builder@openwsn.org>
2014-02-03 04:46:06 -0800

    removing old firmware documentation

--- firmware/structcc1101___a_g_c_c_t_r_l1__reg__t.js
+++
@@ -1,7 +0,0 @@
-var structcc1101___a_g_c_c_t_r_l1__reg__t =
-[
-    [ "AGC_LNA_PRIORITY", "structcc1101___a_g_c_c_t_r_l1__reg__t.html#aa689ccb978449f1219ee3beae5ded69e", null ],
-    [ "CARRIER_SENSE_ABS_THR", "structcc1101___a_g_c_c_t_r_l1__reg__t.html#ad36f8ca39e7597e87b6d7f81845dcafa", null ],
-    [ "CARRIER_SENSE_REL_THR", "structcc1101___a_g_c_c_t_r_l1__reg__t.html#aa8cc7dc610d4f3fc411ed85c468e0baa", null ],
-    [ "unused_r0", "structcc1101___a_g_c_c_t_r_l1__reg__t.html#a31c7762c93075841b4de3a4fc2d5ef81", null ]
-];
--- firmware/structscheduler__dbg__t.js
+++
@@ -1,5 +0,0 @@
-var structscheduler__dbg__t =
-[
-    [ "numTasksCur", "structscheduler__dbg__t.html#a44e0de4a2df834d715a707fdcda4e121", null ],
-    [ "numTasksMax", "structscheduler__dbg__t.html#aea423a6171502584100c420808816db0", null ]
-];
