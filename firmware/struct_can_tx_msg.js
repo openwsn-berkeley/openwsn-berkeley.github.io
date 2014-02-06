bed8d87ffba3eeabd4ea217275f164fdb8c1f4a3
OpenWSN Builder <builder@openwsn.org>
2014-02-06 04:46:17 -0800

    removing old firmware documentation

--- firmware/struct_can_tx_msg.js
+++
@@ -1,9 +0,0 @@
-var struct_can_tx_msg =
-[
-    [ "Data", "struct_can_tx_msg.html#a7a6cef2354d348240c51a3c9f9d66f83", null ],
-    [ "DLC", "struct_can_tx_msg.html#a4a610c088e0557b088340dd06e88434e", null ],
-    [ "ExtId", "struct_can_tx_msg.html#a24faed437c7511e0b47dd48a5560f50f", null ],
-    [ "IDE", "struct_can_tx_msg.html#afe0fb4962ce5ba52620228533832afd3", null ],
-    [ "RTR", "struct_can_tx_msg.html#a4874867352e87c128eb383cdb984abce", null ],
-    [ "StdId", "struct_can_tx_msg.html#a18a9a84e480fe713f345a2f7de127cf2", null ]
-];
