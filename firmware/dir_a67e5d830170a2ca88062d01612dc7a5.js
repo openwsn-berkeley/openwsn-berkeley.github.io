5b19a5bb19ff6c09aaafc1c86adba614cd9046ee
OpenWSN Builder <builder@openwsn.org>
2014-02-09 04:46:44 -0800

    removing old firmware documentation

--- firmware/dir_a67e5d830170a2ca88062d01612dc7a5.js
+++
@@ -1,5 +0,0 @@
-var dir_a67e5d830170a2ca88062d01612dc7a5 =
-[
-    [ "rleds.c", "rleds_8c.html", "rleds_8c" ],
-    [ "rleds.h", "rleds_8h.html", "rleds_8h" ]
-];
--- firmware/wsn430v13b_200std__spi_200std__spi_8c.js
+++
@@ -1,4 +0,0 @@
-var wsn430v13b_200std__spi_200std__spi_8c =
-[
-    [ "main", "wsn430v13b_200std__spi_200std__spi_8c.html#a6288eba0f8e8ad3ab1544ad731eb7667", null ]
-];
