240c1d54374e187ea0dc6217ce095a41a2b36be7
OpenWSN Builder <builder@openwsn.org>
2014-02-14 04:47:22 -0800

    adding new firmware documentation

---
+++ firmware/rinfo_8h.js
@@ -0,0 +1,4 @@
+var rinfo_8h =
+[
+    [ "rinfo_init", "rinfo_8h.html#ga205037164193bf92df86b86cf67b1dd3", null ]
+];
---
+++ firmware/struct_s_s_p___c_f_g___type.js
@@ -0,0 +1,9 @@
+var struct_s_s_p___c_f_g___type =
+[
+    [ "ClockRate", "struct_s_s_p___c_f_g___type.html#aaf211678a520cef3af44d35737f7466e", null ],
+    [ "CPHA", "struct_s_s_p___c_f_g___type.html#aaf6c43320ad2ac7b0bc0dac11374232e", null ],
+    [ "CPOL", "struct_s_s_p___c_f_g___type.html#a816478ce3c6267bc9f66be53517dec36", null ],
+    [ "Databit", "struct_s_s_p___c_f_g___type.html#ae6be88a1e133b08293e77dcdf397709c", null ],
+    [ "FrameFormat", "struct_s_s_p___c_f_g___type.html#a099f32a38e2d169d866e738d746627a0", null ],
+    [ "Mode", "struct_s_s_p___c_f_g___type.html#a0ffc93ec511ed9cf1663f6939bd3e839", null ]
+];
