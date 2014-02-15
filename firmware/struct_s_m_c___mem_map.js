ab7246e61213d2c6dd49d0e55d2b43e7104d9ee1
OpenWSN Builder <builder@openwsn.org>
2014-02-15 04:47:07 -0800

    removing old firmware documentation

--- firmware/struct_s_m_c___mem_map.js
+++
@@ -1,7 +0,0 @@
-var struct_s_m_c___mem_map =
-[
-    [ "PMCTRL", "struct_s_m_c___mem_map.html#acc989af655daf69d506fde822f207e0c", null ],
-    [ "PMPROT", "struct_s_m_c___mem_map.html#a0e48ca7c949ba2fd2f9a29f153ee1245", null ],
-    [ "PMSTAT", "struct_s_m_c___mem_map.html#adacbe2d7cc2808682b7b83e091b850a8", null ],
-    [ "VLLSCTRL", "struct_s_m_c___mem_map.html#a4eaa526fc82a152a0343db20e639d633", null ]
-];
