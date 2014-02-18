f8256ab04426fd9a995f058828a4ec1dc39dcc07
OpenWSN Builder <builder@openwsn.org>
2014-02-18 04:47:05 -0800

    removing old firmware documentation

--- firmware/dir_2cecc73fc64813b2011609d292dbe3a6.js
+++
@@ -1,4 +0,0 @@
-var dir_2cecc73fc64813b2011609d292dbe3a6 =
-[
-    [ "01bsp_debugpins.c", "01bsp__debugpins_8c.html", "01bsp__debugpins_8c" ]
-];
--- firmware/structrsvp__style__t.js
+++
@@ -1,5 +0,0 @@
-var structrsvp__style__t =
-[
-    [ "header", "structrsvp__style__t.html#a24698cfa5f406e4f5e7d1df203cb989c", null ],
-    [ "rsvp_style_t", "structrsvp__style__t.html#a19ab3453ae43e969eb2c82aadbec119f", null ]
-];
