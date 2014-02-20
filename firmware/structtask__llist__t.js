06b1488d55777a9afdf4c27c4140ff0d5b08cb2b
OpenWSN Builder <builder@openwsn.org>
2014-02-20 04:47:35 -0800

    removing old firmware documentation

--- firmware/structtask__llist__t.js
+++
@@ -1,6 +0,0 @@
-var structtask__llist__t =
-[
-    [ "cb", "structtask__llist__t.html#a3198abb70cb27d61d5327c29761a9809", null ],
-    [ "next", "structtask__llist__t.html#a75b19ffcca77bfc647ff02695958fd95", null ],
-    [ "prio", "structtask__llist__t.html#a308195f234d5ba399cbdf35ccd870ea3", null ]
-];
