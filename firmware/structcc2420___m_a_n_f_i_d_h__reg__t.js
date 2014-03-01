61acfe1253cf4da01ae0a24f768f32ef24298066
OpenWSN Builder <builder@openwsn.org>
2014-03-01 04:50:18 -0800

    removing old firmware documentation

--- firmware/structcc2420___m_a_n_f_i_d_h__reg__t.js
+++
@@ -1,5 +0,0 @@
-var structcc2420___m_a_n_f_i_d_h__reg__t =
-[
-    [ "PARTNUM", "structcc2420___m_a_n_f_i_d_h__reg__t.html#aa6f5cd1a8bc6c7bb011e75d9ee2d5c2a", null ],
-    [ "VERSION", "structcc2420___m_a_n_f_i_d_h__reg__t.html#aa38f327ebbabdbd5151512514f401300", null ]
-];
