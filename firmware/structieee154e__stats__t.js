abed3cccd4fc6be30d8b0dfd93dce81597f4d073
OpenWSN Builder <builder@openwsn.org>
2014-03-02 04:49:56 -0800

    removing old firmware documentation

--- firmware/structieee154e__stats__t.js
+++
@@ -1,9 +0,0 @@
-var structieee154e__stats__t =
-[
-    [ "dutyCycle", "structieee154e__stats__t.html#ab5e71ba0276929fe061bacdcc22c3335", null ],
-    [ "maxCorrection", "structieee154e__stats__t.html#a93ce95bb37657a253d66f23e682b074c", null ],
-    [ "minCorrection", "structieee154e__stats__t.html#a35fe3576530c5e0a37b11b93b3f018f0", null ],
-    [ "numDeSync", "structieee154e__stats__t.html#a06c72c5cc3344fbb8dbb7d0ec085d101", null ],
-    [ "numSyncAck", "structieee154e__stats__t.html#afd4db777bc450d6c987acbc71908659f", null ],
-    [ "numSyncPkt", "structieee154e__stats__t.html#a8c069cfb9ee7568f342c7af6a276b3b9", null ]
-];
