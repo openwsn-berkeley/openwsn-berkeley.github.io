73dc61e6e8627a6a67a105e7c3d5f0345f795d8d
OpenWSN Builder <builder@openwsn.org>
2014-03-11 10:42:17 -0700

    removing old firmware documentation

--- firmware/structrtc__timer__vars__t.js
+++
@@ -1,4 +0,0 @@
-var structrtc__timer__vars__t =
-[
-    [ "alarm_cb", "structrtc__timer__vars__t.html#a53b27fd4895472e09dfaa21f7d9ed5d1", null ]
-];
--- firmware/test__large__range__accel_8c.js
+++
@@ -1,8 +0,0 @@
-var test__large__range__accel_8c =
-[
-    [ "main", "test__large__range__accel_8c.html#a6288eba0f8e8ad3ab1544ad731eb7667", null ],
-    [ "large_range_accel_data", "test__large__range__accel_8c.html#ae00ee9ecb8963c7974221c898d43ae6a", null ],
-    [ "large_range_accel_x", "test__large__range__accel_8c.html#a463bde0325fd420852e5e38ad49b9987", null ],
-    [ "large_range_accel_y", "test__large__range__accel_8c.html#a2a94a409d7652ea6d1ec5e44c50dccf1", null ],
-    [ "large_range_accel_z", "test__large__range__accel_8c.html#ad9a6ea89df9a24cb6827520ca5a3d6ab", null ]
-];
