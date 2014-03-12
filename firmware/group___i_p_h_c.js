0c8f2560f7d20eaff1c643b560df1818c78f99d4
OpenWSN Builder <builder@openwsn.org>
2014-03-12 04:44:11 -0700

    adding new firmware documentation

---
+++ firmware/group___i_p_h_c.js
@@ -0,0 +1,116 @@
+var group___i_p_h_c =
+[
+    [ "ipv6_header_iht", "structipv6__header__iht.html", [
+      [ "dest", "structipv6__header__iht.html#ab590828278faf09bd89eafafe0f8057e", null ],
+      [ "flow_label", "structipv6__header__iht.html#a2a57c20fc71dd7e625c322590946c893", null ],
+      [ "header_length", "structipv6__header__iht.html#aa28b14747570c787d6f9c1f06c3cce52", null ],
+      [ "hop_limit", "structipv6__header__iht.html#ae967bb206e3e6ddfcde10d130338a101", null ],
+      [ "next_header", "structipv6__header__iht.html#a045daeeb2d6d7b5c4706eb5ca627e84c", null ],
+      [ "next_header_compressed", "structipv6__header__iht.html#a0d32249571c184f9521c03353863246e", null ],
+      [ "src", "structipv6__header__iht.html#a0b14ac2cf1177c3beabaadf97dc24f47", null ],
+      [ "traffic_class", "structipv6__header__iht.html#af92a6e10a8b9f2e12a072402f1be80c4", null ]
+    ] ],
+    [ "ipv6_hopbyhop_ht", "structipv6__hopbyhop__ht.html", [
+      [ "HdrExtLen", "structipv6__hopbyhop__ht.html#a8f1a42487e5f1593b07d1ac0ba6c9c54", null ],
+      [ "headerlen", "structipv6__hopbyhop__ht.html#ae255ebaa54fa708bb88eface1def600b", null ],
+      [ "lowpan_nhc", "structipv6__hopbyhop__ht.html#ad2678369a74e077f76960440b5182d79", null ],
+      [ "next_header_compressed", "structipv6__hopbyhop__ht.html#a0d32249571c184f9521c03353863246e", null ],
+      [ "nextHeader", "structipv6__hopbyhop__ht.html#a74b50d76e840e2b7381e4778f5dbc12d", null ]
+    ] ],
+    [ "rpl_hopoption_ht", "structrpl__hopoption__ht.html", [
+      [ "flags", "structrpl__hopoption__ht.html#aa2585d779da0ab21273a8d92de9a0ebe", null ],
+      [ "optionLen", "structrpl__hopoption__ht.html#a69d3f389b91aeee94f06f73f81959cc4", null ],
+      [ "optionType", "structrpl__hopoption__ht.html#a4328ee73af1e4c920baca63cb963a9cd", null ],
+      [ "rplInstanceID", "structrpl__hopoption__ht.html#af0dfbc973d21ed95db71ddba97860654", null ],
+      [ "senderRank", "structrpl__hopoption__ht.html#aba4f373b78564b42f515f29b103fefcf", null ]
+    ] ],
+    [ "IPHC_DEFAULT_HOP_LIMIT", "group___i_p_h_c.html#ga320b1d67b93309eeedfa96d2505816c1", null ],
+    [ "IPv6HOP_HDR_LEN", "group___i_p_h_c.html#ga251d45c44deb607ae20f0844cb4428c0", null ],
+    [ "IPHC_CID_enums", "group___i_p_h_c.html#ga751893c0bdb02d7faeb6f52e1bca836f", [
+      [ "IPHC_CID_NO", "group___i_p_h_c.html#gga751893c0bdb02d7faeb6f52e1bca836fa8ae61ca6d607248cc784a62543f97a3e", null ],
+      [ "IPHC_CID_YES", "group___i_p_h_c.html#gga751893c0bdb02d7faeb6f52e1bca836fa3d47c848bcd3cb469b40e3c9ea5c83d5", null ]
+    ] ],
+    [ "IPHC_DAC_enums", "group___i_p_h_c.html#gacadc8526b5031d4739074b1e676825a2", [
+      [ "IPHC_DAC_STATELESS", "group___i_p_h_c.html#ggacadc8526b5031d4739074b1e676825a2a612c8a78f4fe6a80e403716a19f34a83", null ],
+      [ "IPHC_DAC_STATEFUL", "group___i_p_h_c.html#ggacadc8526b5031d4739074b1e676825a2af82a93b7fcd84f5f4d5d86e09a6f00e8", null ]
+    ] ],
+    [ "IPHC_DAM_enums", "group___i_p_h_c.html#ga144ca0928b8496c2c689f8333b1bd1bb", [
+      [ "IPHC_DAM_128B", "group___i_p_h_c.html#gga144ca0928b8496c2c689f8333b1bd1bbafed57a80fb6fc070089992ddd9176035", null ],
+      [ "IPHC_DAM_64B", "group___i_p_h_c.html#gga144ca0928b8496c2c689f8333b1bd1bbadff87ac250ab25343b3d3e8a5d515a26", null ],
+      [ "IPHC_DAM_16B", "group___i_p_h_c.html#gga144ca0928b8496c2c689f8333b1bd1bba54de2119a7aa248bc306c2b580c230fa", null ],
+      [ "IPHC_DAM_ELIDED", "group___i_p_h_c.html#gga144ca0928b8496c2c689f8333b1bd1bbaa8772b0848901f0404edcda4cfcd7f32", null ]
+    ] ],
+    [ "IPHC_DISPATCH_enums", "group___i_p_h_c.html#gaf0b38bf4b9c31af37ee2b843812926b0", [
+      [ "IPHC_DISPATCH_IPHC", "group___i_p_h_c.html#ggaf0b38bf4b9c31af37ee2b843812926b0aa3bd6cfa722537c12a162ab0e8fb16ea", null ]
+    ] ],
+    [ "IPHC_enums", "group___i_p_h_c.html#ga9e5c4749ea128f61e69810fabeb67447", [
+      [ "IPHC_DISPATCH", "group___i_p_h_c.html#gga9e5c4749ea128f61e69810fabeb67447ab3e9332267b398837a6d6f3b05bab978", null ],
+      [ "IPHC_TF", "group___i_p_h_c.html#gga9e5c4749ea128f61e69810fabeb67447a97d2943962ddd101a4a0ff32d172e585", null ],
+      [ "IPHC_NH", "group___i_p_h_c.html#gga9e5c4749ea128f61e69810fabeb67447a357a6bdff6dffbc5694eca168e9203bd", null ],
+      [ "IPHC_HLIM", "group___i_p_h_c.html#gga9e5c4749ea128f61e69810fabeb67447a69c9510ea7b5895418cb7a282a4b22cc", null ],
+      [ "IPHC_CID", "group___i_p_h_c.html#gga9e5c4749ea128f61e69810fabeb67447aa78bfcd0c59158eeddfd129f4d3ae251", null ],
+      [ "IPHC_SAC", "group___i_p_h_c.html#gga9e5c4749ea128f61e69810fabeb67447a4ca5f67efa97d9936a7f905358f67a7c", null ],
+      [ "IPHC_SAM", "group___i_p_h_c.html#gga9e5c4749ea128f61e69810fabeb67447a5ee0a940c912935cc96000f85b32315e", null ],
+      [ "IPHC_M", "group___i_p_h_c.html#gga9e5c4749ea128f61e69810fabeb67447a6fd20bd31b89564eb43946e608ee6a85", null ],
+      [ "IPHC_DAC", "group___i_p_h_c.html#gga9e5c4749ea128f61e69810fabeb67447aeadc9cccfe3951fa08d2370185618d1a", null ],
+      [ "IPHC_DAM", "group___i_p_h_c.html#gga9e5c4749ea128f61e69810fabeb67447aa39679c15ab106fe76e8c82caa12283e", null ]
+    ] ],
+    [ "IPHC_HLIM_enums", "group___i_p_h_c.html#ga189f4a88715e326fed26365ced5c4f92", [
+      [ "IPHC_HLIM_INLINE", "group___i_p_h_c.html#gga189f4a88715e326fed26365ced5c4f92a5c12c89dad46a729f9b32d63508d96fe", null ],
+      [ "IPHC_HLIM_1", "group___i_p_h_c.html#gga189f4a88715e326fed26365ced5c4f92ae141dec6b14fb4a6bc8fdcf6893cfa2d", null ],
+      [ "IPHC_HLIM_64", "group___i_p_h_c.html#gga189f4a88715e326fed26365ced5c4f92a5f6b02f479096bef700764db225b8d26", null ],
+      [ "IPHC_HLIM_255", "group___i_p_h_c.html#gga189f4a88715e326fed26365ced5c4f92a9d7d373c07a2a140034b1ae398fefd3d", null ]
+    ] ],
+    [ "IPHC_M_enums", "group___i_p_h_c.html#ga64b8fcf888c919c2d13719ffcf64d322", [
+      [ "IPHC_M_NO", "group___i_p_h_c.html#gga64b8fcf888c919c2d13719ffcf64d322ae9ede293c533f5defc46e753b2498fc7", null ],
+      [ "IPHC_M_YES", "group___i_p_h_c.html#gga64b8fcf888c919c2d13719ffcf64d322a81ee2de7c011153decc8668176d6eeb5", null ]
+    ] ],
+    [ "IPHC_NH_enums", "group___i_p_h_c.html#gaf0d239d3843a392a4a775ba41471328f", [
+      [ "IPHC_NH_INLINE", "group___i_p_h_c.html#ggaf0d239d3843a392a4a775ba41471328fa36eb5863fcf587436e0d54fce7a9dc8a", null ],
+      [ "IPHC_NH_COMPRESSED", "group___i_p_h_c.html#ggaf0d239d3843a392a4a775ba41471328fa0d4cc09ac8fbbac8b95bb04a3f1d02e8", null ]
+    ] ],
+    [ "IPHC_SAC_enums", "group___i_p_h_c.html#ga54eb4faf7603cb807d120898540e176f", [
+      [ "IPHC_SAC_STATELESS", "group___i_p_h_c.html#gga54eb4faf7603cb807d120898540e176faf5419386cb8ee9c68cfc802947b434ff", null ],
+      [ "IPHC_SAC_STATEFUL", "group___i_p_h_c.html#gga54eb4faf7603cb807d120898540e176fa4eb24f475940455398825394450cab73", null ]
+    ] ],
+    [ "IPHC_SAM_enums", "group___i_p_h_c.html#ga03418e20cce3d34c0bd55809817fae8f", [
+      [ "IPHC_SAM_128B", "group___i_p_h_c.html#gga03418e20cce3d34c0bd55809817fae8fa36ed450492577a63f390d3bf051c8984", null ],
+      [ "IPHC_SAM_64B", "group___i_p_h_c.html#gga03418e20cce3d34c0bd55809817fae8fac47b1e72e62dc19c31260f3b6d00b086", null ],
+      [ "IPHC_SAM_16B", "group___i_p_h_c.html#gga03418e20cce3d34c0bd55809817fae8fac5d7c07eb7725e9e623b4e27b44c422a", null ],
+      [ "IPHC_SAM_ELIDED", "group___i_p_h_c.html#gga03418e20cce3d34c0bd55809817fae8fa4cd47abd194642036099abd97087474e", null ]
+    ] ],
+    [ "IPHC_TF_enums", "group___i_p_h_c.html#gabd1bc92f5f7189d1c1a69f1dedf5b3bf", [
+      [ "IPHC_TF_4B", "group___i_p_h_c.html#ggabd1bc92f5f7189d1c1a69f1dedf5b3bfaae62006a5cf0db404b37ce4b678624fc", null ],
+      [ "IPHC_TF_3B", "group___i_p_h_c.html#ggabd1bc92f5f7189d1c1a69f1dedf5b3bfa0a1286a945cfb0c1e73828ad82b93107", null ],
+      [ "IPHC_TF_1B", "group___i_p_h_c.html#ggabd1bc92f5f7189d1c1a69f1dedf5b3bfaec93d3dab5aa46de6542a3526ce78356", null ],
+      [ "IPHC_TF_ELIDED", "group___i_p_h_c.html#ggabd1bc92f5f7189d1c1a69f1dedf5b3bfae3d60430172746107f077a8b6cfca159", null ]
+    ] ],
+    [ "NHC_enums", "group___i_p_h_c.html#ga1a87c0dc18c19390f31c5c79af59c751", [
+      [ "NHC_IPv6EXT_MASK", "group___i_p_h_c.html#gga1a87c0dc18c19390f31c5c79af59c751ac63d7b6b4f5243bd13818e4f1fd4fd21", null ],
+      [ "NHC_IPv6EXT_ID", "group___i_p_h_c.html#gga1a87c0dc18c19390f31c5c79af59c751ac23ad0b45591c41e9300c403b7a42640", null ],
+      [ "NHC_UDP_MASK", "group___i_p_h_c.html#gga1a87c0dc18c19390f31c5c79af59c751a11b631a6027548a2661af17422b0ff95", null ],
+      [ "NHC_UDP_ID", "group___i_p_h_c.html#gga1a87c0dc18c19390f31c5c79af59c751a38f99c48ee29e413ed0c62bcd9fe5107", null ]
+    ] ],
+    [ "NHC_IPv6HOP_enums", "group___i_p_h_c.html#ga5f822530a45bc4389c88ad1e7100f31a", [
+      [ "NHC_IPv6HOP_MASK", "group___i_p_h_c.html#gga5f822530a45bc4389c88ad1e7100f31aa723d3eb60405556dd29b83f59ef268f9", null ],
+      [ "NHC_IPv6HOP_VAL", "group___i_p_h_c.html#gga5f822530a45bc4389c88ad1e7100f31aacce2b9e9b190ef0ac4975a8c04757f10", null ],
+      [ "NHC_HOP_NH_MASK", "group___i_p_h_c.html#gga5f822530a45bc4389c88ad1e7100f31aa402156e5d5bdd8471bb53df2cd152ff7", null ]
+    ] ],
+    [ "NHC_UDP_enums", "group___i_p_h_c.html#ga7afe46dd099effec3f504f29bb10c4fe", [
+      [ "NHC_UDP_C_MASK", "group___i_p_h_c.html#gga7afe46dd099effec3f504f29bb10c4fea4711e5b0ae521173d249943bb24717ca", null ],
+      [ "NHC_UDP_PORTS_MASK", "group___i_p_h_c.html#gga7afe46dd099effec3f504f29bb10c4fea455c2dbb80da88e48b77a69348eebb19", null ]
+    ] ],
+    [ "NHC_UDP_PORTS_enums", "group___i_p_h_c.html#ga2ccc80e9c7095920e9f24682b29933dd", [
+      [ "NHC_UDP_PORTS_INLINE", "group___i_p_h_c.html#gga2ccc80e9c7095920e9f24682b29933ddac6189b2b203d79aa5c10b9a32f63af90", null ],
+      [ "NHC_UDP_PORTS_16S_8D", "group___i_p_h_c.html#gga2ccc80e9c7095920e9f24682b29933ddab29a07ce6359ca61e907b8495927b57c", null ],
+      [ "NHC_UDP_PORTS_8S_8D", "group___i_p_h_c.html#gga2ccc80e9c7095920e9f24682b29933dda1f572e3350847dd34b396e37e300bde4", null ],
+      [ "NHC_UDP_PORTS_4S_4D", "group___i_p_h_c.html#gga2ccc80e9c7095920e9f24682b29933ddab3447deb5ed8e54d6bc352e3ede6ec4a", null ]
+    ] ],
+    [ "iphc_init", "group___i_p_h_c.html#ga0f7e6c70cc55b45506d36a367afce6e9", null ],
+    [ "iphc_receive", "group___i_p_h_c.html#ga1d37923eb55e392d0a8a8e315a941e02", null ],
+    [ "iphc_sendDone", "group___i_p_h_c.html#ga74c2778b882cbb1be841b5e31e8d27ee", null ],
+    [ "iphc_sendFromBridge", "group___i_p_h_c.html#ga4d3398508cb6263929b37fc238c829f6", null ],
+    [ "iphc_sendFromForwarding", "group___i_p_h_c.html#ga7a000f74b25d6cbcb76265c4cb3e2517", null ],
+    [ "PRAGMA", "group___i_p_h_c.html#ga44f3d4b22a86e1e2a6dbc81bd4c92be1", null ],
+    [ "PRAGMA", "group___i_p_h_c.html#ga383707c8cf57a58ab67888eb545034dd", null ]
+];
