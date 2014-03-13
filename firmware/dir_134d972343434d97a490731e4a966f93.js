926f2e1ee7fded4cbe215598ce510046836c8807
OpenWSN Builder <builder@openwsn.org>
2014-03-13 04:43:42 -0700

    removing old firmware documentation

--- firmware/dir_134d972343434d97a490731e4a966f93.js
+++
@@ -1,5 +0,0 @@
-var dir_134d972343434d97a490731e4a966f93 =
-[
-    [ "udpecho.c", "udpecho_8c.html", "udpecho_8c" ],
-    [ "udpecho.h", "udpecho_8h.html", "udpecho_8h" ]
-];
--- firmware/gmpls_8c.js
+++
@@ -1,9 +0,0 @@
-var gmpls_8c =
-[
-    [ "mpls_vars_t", "structmpls__vars__t.html", "structmpls__vars__t" ],
-    [ "mpls_init", "gmpls_8c.html#a6ede47092128f29048a05acf51be29a7", null ],
-    [ "mpls_receive", "gmpls_8c.html#a0b54eecb39ddf1a1bd95c2926f33ecac", null ],
-    [ "mpls_send", "gmpls_8c.html#ae3cef9cec8745bbd469918877b65f5bf", null ],
-    [ "mpls_timer_cb", "gmpls_8c.html#a410d527a8ff07212758b1a33bf48856f", null ],
-    [ "mpls_vars", "gmpls_8c.html#af7a3eae839f26d76e9751018e7b522af", null ]
-];
