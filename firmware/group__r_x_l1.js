9b642b7902dc1e83dc4f3f585c1c3c4ba0d7e8dd
OpenWSN Builder <builder@openwsn.org>
2014-03-18 04:43:40 -0700

    adding new firmware documentation

---
+++ firmware/group__r_x_l1.js
@@ -0,0 +1,5 @@
+var group__r_x_l1 =
+[
+    [ "rinfo_init", "group__r_x_l1.html#ga205037164193bf92df86b86cf67b1dd3", null ],
+    [ "rxl1_init", "group__r_x_l1.html#ga4a004d71ee3106152559c6bd214ec97f", null ]
+];
---
+++ firmware/sctimer_8h.js
@@ -0,0 +1,12 @@
+var sctimer_8h =
+[
+    [ "SCTIMER_TICS_MS", "sctimer_8h.html#ga08e4c399d2058414497e57a37033eaf0", null ],
+    [ "sctimer_cbt", "sctimer_8h.html#ga5370fd17fe059c77c1a9bf514fdc2970", null ],
+    [ "sctimer_clearISR", "sctimer_8h.html#ga50a19c77f132eaf36b324d6740cfe6c1", null ],
+    [ "sctimer_getValue", "sctimer_8h.html#ga41ba1f04048a657262fd76bfba3e0b0c", null ],
+    [ "sctimer_init", "sctimer_8h.html#ga7b243ad7df9e6f570b94d765b0506d6e", null ],
+    [ "sctimer_reset", "sctimer_8h.html#ga6780e78751e8ad88c819beeb506d9434", null ],
+    [ "sctimer_schedule", "sctimer_8h.html#ga75c5abd9a9fd75879e4add013048fb69", null ],
+    [ "sctimer_setCb", "sctimer_8h.html#gac5a05074749b0e138c23e0342bad5690", null ],
+    [ "sctimer_stop", "sctimer_8h.html#gafd788db70f8988214af69cd01bea6f41", null ]
+];
