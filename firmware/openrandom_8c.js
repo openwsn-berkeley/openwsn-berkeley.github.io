0dc612658847bd5d483313c6fdca13fe0a2cc853
OpenWSN Builder <builder@openwsn.org>
2014-03-21 04:43:39 -0700

    removing old firmware documentation

--- firmware/openrandom_8c.js
+++
@@ -1,6 +0,0 @@
-var openrandom_8c =
-[
-    [ "openrandom_get16b", "openrandom_8c.html#gaafd337b51477d5eeb5ae0d6489bb69e6", null ],
-    [ "openrandom_init", "openrandom_8c.html#ga226a338ea5cd81f0b9e92584540125a5", null ],
-    [ "random_vars", "openrandom_8c.html#ae436857566b6ad035ea13f70248fa740", null ]
-];
--- firmware/structudpstorm__vars__t.js
+++
@@ -1,6 +0,0 @@
-var structudpstorm__vars__t =
-[
-    [ "desc", "structudpstorm__vars__t.html#af824c41339fef8ae82ca38c2ddc4eab8", null ],
-    [ "seqNum", "structudpstorm__vars__t.html#a17c912858554a9fcdf7ba2dc5847674c", null ],
-    [ "timerId", "structudpstorm__vars__t.html#a1952925d51e375d4da096207f03a8777", null ]
-];
