1ce9a722173c191c4f7d4cb0167f0c596e93c453
OpenWSN Builder <builder@openwsn.org>
2014-03-24 04:43:54 -0700

    removing old firmware documentation

--- firmware/structrex__vars__t.js
+++
@@ -1,5 +0,0 @@
-var structrex__vars__t =
-[
-    [ "desc", "structrex__vars__t.html#af824c41339fef8ae82ca38c2ddc4eab8", null ],
-    [ "timerId", "structrex__vars__t.html#a1952925d51e375d4da096207f03a8777", null ]
-];
