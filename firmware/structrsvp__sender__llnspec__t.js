007e0269c0114e470e5de559673b8a01ae119dd3
OpenWSN Builder <builder@openwsn.org>
2014-03-27 04:43:44 -0700

    removing old firmware documentation

--- firmware/structrsvp__sender__llnspec__t.js
+++
@@ -1,7 +0,0 @@
-var structrsvp__sender__llnspec__t =
-[
-    [ "demand", "structrsvp__sender__llnspec__t.html#adf1652ef23b09d6b8bce80646e9110c6", null ],
-    [ "ehmode", "structrsvp__sender__llnspec__t.html#ad080ff9e462c1b99c01eac0a1d32d481", null ],
-    [ "header", "structrsvp__sender__llnspec__t.html#a24698cfa5f406e4f5e7d1df203cb989c", null ],
-    [ "total_demand", "structrsvp__sender__llnspec__t.html#a1e1add8b8659ddba8d8349bee54b5dfb", null ]
-];
