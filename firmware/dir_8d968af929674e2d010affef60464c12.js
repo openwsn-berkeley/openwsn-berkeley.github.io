ea3f4fe8bff9b371426a173d5ce97f34495ec742
OpenWSN Builder <builder@openwsn.org>
2014-04-01 23:34:04 -0700

    removing old firmware documentation

--- firmware/dir_8d968af929674e2d010affef60464c12.js
+++
@@ -1,7 +0,0 @@
-var dir_8d968af929674e2d010affef60464c12 =
-[
-    [ "ohlone.c", "ohlone_8c.html", "ohlone_8c" ],
-    [ "ohlone.h", "ohlone_8h.html", "ohlone_8h" ],
-    [ "ohlone_webpages.c", "ohlone__webpages_8c.html", "ohlone__webpages_8c" ],
-    [ "ohlone_webpages.h", "ohlone__webpages_8h.html", "ohlone__webpages_8h" ]
-];
--- firmware/rinfo_8h.js
+++
@@ -1,4 +0,0 @@
-var rinfo_8h =
-[
-    [ "rinfo_init", "rinfo_8h.html#ga205037164193bf92df86b86cf67b1dd3", null ]
-];
