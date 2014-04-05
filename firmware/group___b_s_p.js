1a32db45b69ab1a5ba6dae25dd3bcaaecda7fc21
OpenWSN Builder <builder@openwsn.org>
2014-04-05 04:45:00 -0700

    adding new firmware documentation

---
+++ firmware/group___b_s_p.js
@@ -0,0 +1,12 @@
+var group___b_s_p =
+[
+    [ "Board", "group__board.html", "group__board" ],
+    [ "Bsp_timer", "group__bsp__timer.html", "group__bsp__timer" ],
+    [ "Debugpins", "group__debugpins.html", "group__debugpins" ],
+    [ "Eui64", "group__eui64.html", "group__eui64" ],
+    [ "Leds", "group__leds.html", "group__leds" ],
+    [ "Radio", "group__radio.html", "group__radio" ],
+    [ "Radiotimer", "group__radiotimer.html", "group__radiotimer" ],
+    [ "Sctimer", "group__sctimer.html", "group__sctimer" ],
+    [ "Uart", "group__uart.html", "group__uart" ]
+];
---
+++ firmware/structopentimers__t.js
@@ -0,0 +1,10 @@
+var structopentimers__t =
+[
+    [ "callback", "structopentimers__t.html#a3461cd0530b69e2ce762343ab891d1dc", null ],
+    [ "hasExpired", "structopentimers__t.html#a877a0357dbada39beadafda5e29931c1", null ],
+    [ "isrunning", "structopentimers__t.html#aea10ec4f88ded65df5fd3f4b5cd8b6f1", null ],
+    [ "period_ticks", "structopentimers__t.html#a4986b5cccef243532e7fd298036bb163", null ],
+    [ "ticks_remaining", "structopentimers__t.html#a0f8dc228274620a10c7288e7c7d73e2a", null ],
+    [ "type", "structopentimers__t.html#a7171f46f3e9c1cd6e544ffe1b94727e8", null ],
+    [ "wraps_remaining", "structopentimers__t.html#aaaa68559efdf18737d96ff6857a4a4d2", null ]
+];
