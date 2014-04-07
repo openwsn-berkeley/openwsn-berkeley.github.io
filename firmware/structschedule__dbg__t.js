2d43471c79da319c2177a3b72f049f0316e19254
OpenWSN Builder <builder@openwsn.org>
2014-04-07 04:45:10 -0700

    adding new firmware documentation

---
+++ firmware/structschedule__dbg__t.js
@@ -0,0 +1,6 @@
+var structschedule__dbg__t =
+[
+    [ "numActiveSlotsCur", "structschedule__dbg__t.html#af519bba16ab3ff747ceaa68e11b1586d", null ],
+    [ "numActiveSlotsMax", "structschedule__dbg__t.html#a3e3e7f6f35c42047f0ce5747100c7ddd", null ],
+    [ "numUpdatedSlotsCur", "structschedule__dbg__t.html#a13c5278ca0a40eabf33a385d1ee5735b", null ]
+];
