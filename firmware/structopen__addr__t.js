ade3c42893a736f3058744b08d23fcb88267197b
OpenWSN Builder <builder@openwsn.org>
2014-04-07 04:44:54 -0700

    removing old firmware documentation

--- firmware/structopen__addr__t.js
+++
@@ -1,9 +0,0 @@
-var structopen__addr__t =
-[
-    [ "addr_128b", "structopen__addr__t.html#a91042bccd78127ff1f8fd516397de2ea", null ],
-    [ "addr_16b", "structopen__addr__t.html#af1c1fe5e7aeb4ebc62aa9062d769903e", null ],
-    [ "addr_64b", "structopen__addr__t.html#a7883e2f04b296c641bc4c24de62d6387", null ],
-    [ "panid", "structopen__addr__t.html#a612ff96ce0dfc46d47fc936ad4a06110", null ],
-    [ "prefix", "structopen__addr__t.html#a99868b72ad64cfbc2c7f5c645e79fd3b", null ],
-    [ "type", "structopen__addr__t.html#a1d127017fb298b889f4ba24752d08b8e", null ]
-];
