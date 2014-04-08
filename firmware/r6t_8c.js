27ad4c99e9b4db24f45fbac4adc681cba5f0cca4
OpenWSN Builder <builder@openwsn.org>
2014-04-08 04:45:04 -0700

    adding new firmware documentation

---
+++ firmware/r6t_8c.js
@@ -0,0 +1,8 @@
+var r6t_8c =
+[
+    [ "r6t_init", "r6t_8c.html#gab57fa4ba158b210a17a992a85e71a437", null ],
+    [ "r6t_receive", "r6t_8c.html#a86051f98fb3d27f3c044d55505cd17bb", null ],
+    [ "r6t_sendDone", "r6t_8c.html#a3a23e2a1f2cd0d8b078c22b2ad0790cc", null ],
+    [ "r6t_path0", "r6t_8c.html#ad0e3b38f2e869610d0d740a612966b24", null ],
+    [ "r6t_vars", "r6t_8c.html#a848739d33de539785334a16a3300e45d", null ]
+];
---
+++ firmware/structneighbor_row__t.js
@@ -0,0 +1,16 @@
+var structneighbor_row__t =
+[
+    [ "addr_64b", "structneighbor_row__t.html#a0c5a1796891070944ae492fb732017c7", null ],
+    [ "asn", "structneighbor_row__t.html#ae0d42a38f9f6a63cda898e7971df742e", null ],
+    [ "DAGrank", "structneighbor_row__t.html#a1b9fd98f0625d88f4dfd23af7ecf590f", null ],
+    [ "joinPrio", "structneighbor_row__t.html#a2d0606b98bb6be3aa5b934654a2c68b1", null ],
+    [ "numRx", "structneighbor_row__t.html#ab633a48aed6e6248411c9cb4b4d309cc", null ],
+    [ "numTx", "structneighbor_row__t.html#a80785a5e701e393a549c13704aeb4905", null ],
+    [ "numTxACK", "structneighbor_row__t.html#a86bffffaee99f71b951a0f8ee7574c7a", null ],
+    [ "numWraps", "structneighbor_row__t.html#ac61321d7ff132bd409faccd66cbabad0", null ],
+    [ "parentPreference", "structneighbor_row__t.html#ad2c1a394ed19cf899e54c2a2867fc3d0", null ],
+    [ "rssi", "structneighbor_row__t.html#a3b962e67ba74725bd60ca3c29f785abe", null ],
+    [ "stableNeighbor", "structneighbor_row__t.html#a441047d629ba5f4fa3e36c20925c0dce", null ],
+    [ "switchStabilityCounter", "structneighbor_row__t.html#af7983be2e9ec469b8588c0a89b536141", null ],
+    [ "used", "structneighbor_row__t.html#accd366cbfd7b8a1b904ca4177a894f95", null ]
+];
