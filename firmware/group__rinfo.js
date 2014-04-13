bb97ab962b3868499b4f37921bbc8ab0be9e9f3b
OpenWSN Builder <builder@openwsn.org>
2014-04-13 04:46:56 -0700

    adding new firmware documentation

---
+++ firmware/group__rinfo.js
@@ -0,0 +1,7 @@
+var group__rinfo =
+[
+    [ "rinfo_vars_t", "structrinfo__vars__t.html", [
+      [ "desc", "structrinfo__vars__t.html#af824c41339fef8ae82ca38c2ddc4eab8", null ]
+    ] ],
+    [ "rinfo_init", "group__rinfo.html#ga205037164193bf92df86b86cf67b1dd3", null ]
+];
