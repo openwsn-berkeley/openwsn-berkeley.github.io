c814f6c9f599829c078a8cb6439dc8c6c5550f98
OpenWSN Builder <builder@openwsn.org>
2014-04-15 05:04:08 -0700

    adding new firmware documentation

---
+++ firmware/group__r_well_known.js
@@ -0,0 +1,7 @@
+var group__r_well_known =
+[
+    [ "rwellknown_vars_t", "structrwellknown__vars__t.html", [
+      [ "desc", "structrwellknown__vars__t.html#af824c41339fef8ae82ca38c2ddc4eab8", null ]
+    ] ],
+    [ "rwellknown_init", "group__r_well_known.html#gae32a9034452d7dc868b9e5f57e002a22", null ]
+];
