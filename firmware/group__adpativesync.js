06e633fee6609c659e8b677b37003af1ef749fca
OpenWSN Builder <builder@openwsn.org>
2014-04-18 04:45:07 -0700

    adding new firmware documentation

---
+++ firmware/group__adpativesync.js
@@ -0,0 +1,29 @@
+var group__adpativesync =
+[
+    [ "compensationInfo_t", "structcompensation_info__t.html", [
+      [ "compensationSlots", "structcompensation_info__t.html#a4583a6a866fca4fe5656e4cabd74c765", null ],
+      [ "neighborID", "structcompensation_info__t.html#a9e544af4973ad7465e99d468575983c2", null ]
+    ] ],
+    [ "adaptive_sync_vars_t", "structadaptive__sync__vars__t.html", [
+      [ "clockState", "structadaptive__sync__vars__t.html#a2ae6da49f0aa41437a46d7664a791328", null ],
+      [ "compensateThreshold", "structadaptive__sync__vars__t.html#a1cf27204061059a4df2952487dfbf956", null ],
+      [ "compensateTicks", "structadaptive__sync__vars__t.html#a22e901e9164a2cf3156202b85ea13c13", null ],
+      [ "compensationInfo_vars", "structadaptive__sync__vars__t.html#a7ea381a05f5c3a88a496eec24e99b01f", null ],
+      [ "compensationTimeout", "structadaptive__sync__vars__t.html#aff1e3c6e088c18e039ddb25f968b38e4", null ],
+      [ "driftChanged", "structadaptive__sync__vars__t.html#a23ff1e4e9caeb263779c2006aaa9f961", null ],
+      [ "elapsedSlots", "structadaptive__sync__vars__t.html#a29b6cab78a42d764ceffbe5de199e5b1", null ],
+      [ "oldASN", "structadaptive__sync__vars__t.html#a1667738b9b7d06ef21837f94d22dcbfd", null ],
+      [ "sumOfTC", "structadaptive__sync__vars__t.html#a481f18bbe789a1e6bfba445f731fa899", null ]
+    ] ],
+    [ "adaptive_sync_state_t", "group__adpativesync.html#gac81060fb836128f6649b1249f2eca999", [
+      [ "S_NONE", "group__adpativesync.html#ggac81060fb836128f6649b1249f2eca999acb5ec123e88fa0c9a1548d1f05b03e0f", null ],
+      [ "S_FASTER", "group__adpativesync.html#ggac81060fb836128f6649b1249f2eca999a04968eb2fb707873f1ef525cc2dcdaca", null ],
+      [ "S_SLOWER", "group__adpativesync.html#ggac81060fb836128f6649b1249f2eca999a9687b49b1490ae284f1be634573de4ee", null ]
+    ] ],
+    [ "adaptive_sync_calculateCompensatedSlots", "group__adpativesync.html#ga4754d7eb03173c9d65256a04abbdd3a1", null ],
+    [ "adaptive_sync_countCompensationTimeout", "group__adpativesync.html#gacf9f3f07c3daf577f34b5e80584dc25f", null ],
+    [ "adaptive_sync_countCompensationTimeout_compoundSlots", "group__adpativesync.html#gadfc86247876d1706bb652832d8dd4cc5", null ],
+    [ "adaptive_sync_driftChanged", "group__adpativesync.html#gadff0464cf68a29739ec724c7be9d311e", null ],
+    [ "adaptive_sync_init", "group__adpativesync.html#gaef3ef6115193e6519adf836f433e77c8", null ],
+    [ "adaptive_sync_preprocess", "group__adpativesync.html#ga8425186666bdbb057c06492731509d60", null ]
+];
