2318a730f27c49317c3be8265238cd177bec93ad
OpenWSN Builder <builder@openwsn.org>
2014-04-22 04:45:04 -0700

    removing old firmware documentation

--- firmware/group__r_x_l1.js
+++
@@ -1,4 +0,0 @@
-var group__r_x_l1 =
-[
-    [ "rxl1_init", "group__r_x_l1.html#ga4a004d71ee3106152559c6bd214ec97f", null ]
-];
--- firmware/icmpv6echo_8c.js
+++
@@ -1,8 +0,0 @@
-var icmpv6echo_8c =
-[
-    [ "icmpv6echo_init", "icmpv6echo_8c.html#ga03c31f22ba17e84f7c1049a660a972fb", null ],
-    [ "icmpv6echo_receive", "icmpv6echo_8c.html#gaa78a139f3dd544c9a78e5c7397419408", null ],
-    [ "icmpv6echo_sendDone", "icmpv6echo_8c.html#ga0502bcb8a36b276660fd1b4b40939753", null ],
-    [ "icmpv6echo_trigger", "icmpv6echo_8c.html#ga153bb13f09671a437cb3da4565ef7586", null ],
-    [ "icmpv6echo_vars", "icmpv6echo_8c.html#a4c8e23abe85626fc7f8b65a134719346", null ]
-];
