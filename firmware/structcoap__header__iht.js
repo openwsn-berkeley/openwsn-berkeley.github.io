ca70d0092bf1c6e6ebc014ff93a2c878337eb203
OpenWSN Builder <builder@openwsn.org>
2014-04-24 04:44:17 -0700

    removing old firmware documentation

--- firmware/structcoap__header__iht.js
+++
@@ -1,9 +0,0 @@
-var structcoap__header__iht =
-[
-    [ "Code", "structcoap__header__iht.html#aa72b8de1e63d9d5670bf65c7edbac73d", null ],
-    [ "messageID", "structcoap__header__iht.html#a48b8857a8afbd63a6aa95cea074e9ca5", null ],
-    [ "T", "structcoap__header__iht.html#a3af1fb0dc6e28d948139547984511081", null ],
-    [ "TKL", "structcoap__header__iht.html#ae729c461e1ed105c2f47d9a706fb2720", null ],
-    [ "token", "structcoap__header__iht.html#a914f56298e55ee7cd7e9e284467e1dfb", null ],
-    [ "Ver", "structcoap__header__iht.html#ab3044e7fa4830934d1b274d1bb2a2834", null ]
-];
