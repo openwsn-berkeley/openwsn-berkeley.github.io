8553a538f515c8f338e8451b143e99635ca760c9
OpenWSN Builder <builder@openwsn.org>
2014-04-26 04:44:45 -0700

    removing old firmware documentation

--- firmware/icmpv6_8h.js
+++
@@ -1,7 +0,0 @@
-var icmpv6_8h =
-[
-    [ "icmpv6_init", "icmpv6_8h.html#gafa1985173c796a6cdd6a66ec6e97a0d7", null ],
-    [ "icmpv6_receive", "icmpv6_8h.html#ga2b5a2c1c1f7e47888b32e19f973d3953", null ],
-    [ "icmpv6_send", "icmpv6_8h.html#ga3415aa981903be4fee8052a07e87bd7f", null ],
-    [ "icmpv6_sendDone", "icmpv6_8h.html#gae528a32af88598b7f56514e3c1ddea3e", null ]
-];
