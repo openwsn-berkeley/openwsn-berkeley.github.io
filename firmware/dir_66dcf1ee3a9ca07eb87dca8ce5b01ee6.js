86d6350c8a8b2771fe86147996b2747065f568ab
OpenWSN Builder <builder@openwsn.org>
2014-04-30 04:44:07 -0700

    removing old firmware documentation

--- firmware/dir_66dcf1ee3a9ca07eb87dca8ce5b01ee6.js
+++
@@ -1,7 +0,0 @@
-var dir_66dcf1ee3a9ca07eb87dca8ce5b01ee6 =
-[
-    [ "iphc.c", "iphc_8c.html", "iphc_8c" ],
-    [ "iphc.h", "iphc_8h.html", "iphc_8h" ],
-    [ "openbridge.c", "openbridge_8c.html", "openbridge_8c" ],
-    [ "openbridge.h", "openbridge_8h.html", "openbridge_8h" ]
-];
