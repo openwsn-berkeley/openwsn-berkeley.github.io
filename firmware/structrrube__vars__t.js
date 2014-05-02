dff8ea55302aa67f1f36a4d16bf5eee9c77601cf
OpenWSN Builder <builder@openwsn.org>
2014-05-02 04:44:31 -0700

    adding new firmware documentation

---
+++ firmware/structrrube__vars__t.js
@@ -0,0 +1,7 @@
+var structrrube__vars__t =
+[
+    [ "desc", "structrrube__vars__t.html#af824c41339fef8ae82ca38c2ddc4eab8", null ],
+    [ "nextHop", "structrrube__vars__t.html#ae1fcf84aa4ef95f95a819f95a2976f18", null ],
+    [ "rrube_state", "structrrube__vars__t.html#a08abdf35fd488d147bd897dffc1488fa", null ],
+    [ "timerId", "structrrube__vars__t.html#a1952925d51e375d4da096207f03a8777", null ]
+];
