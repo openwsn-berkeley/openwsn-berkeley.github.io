c766f635ebacdca16b9550ed31e299beb9c2664e
OpenWSN Builder <builder@openwsn.org>
2014-05-03 04:44:28 -0700

    removing old firmware documentation

--- firmware/structieee154e__dbg__t.js
+++
@@ -1,7 +0,0 @@
-var structieee154e__dbg__t =
-[
-    [ "num_endOfFrame", "structieee154e__dbg__t.html#a3b8f59a1e04f1069feeb6f482be8df30", null ],
-    [ "num_newSlot", "structieee154e__dbg__t.html#a1ca990166d979954eed1c16f3abf4652", null ],
-    [ "num_startOfFrame", "structieee154e__dbg__t.html#a91de02a2ca85a5861eea2735e816284d", null ],
-    [ "num_timer", "structieee154e__dbg__t.html#acc3e5eab3780263299e5dd454e9b988b", null ]
-];
