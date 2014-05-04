ed5b6454ce6b9d8b764e4303fbfccb759be6b5b0
OpenWSN Builder <builder@openwsn.org>
2014-05-04 04:44:18 -0700

    removing old firmware documentation

--- firmware/r6tus_8c.js
+++
@@ -1,9 +0,0 @@
-var r6tus_8c =
-[
-    [ "TSCH_GET_OPTIONS", "r6tus_8c.html#a0ef7ec5e35bab72ab0b348d4f0f2ca47", null ],
-    [ "r6tus_init", "r6tus_8c.html#ga7d0fb5db34ebddc73f5c6738f1276949", null ],
-    [ "r6tus_receive", "r6tus_8c.html#ac50364e3f1b2151ea4d905c230af9fad", null ],
-    [ "r6tus_sendDone", "r6tus_8c.html#adc091122b9f3ca4b1c63405ff22e0eed", null ],
-    [ "r6tus_path0", "r6tus_8c.html#a7585811856d16ca3193e8d29fa47d8c3", null ],
-    [ "r6tus_vars", "r6tus_8c.html#abb9eb3f148f864e948a4c0424799a7ab", null ]
-];
