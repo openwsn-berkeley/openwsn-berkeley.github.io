409f5fb06f1c51e12fbc76ea89e2a5b099b71ab6
OpenWSN Builder <builder@openwsn.org>
2014-05-09 04:44:41 -0700

    adding new firmware documentation

---
+++ firmware/group___open_bridge.js
@@ -0,0 +1,7 @@
+var group___open_bridge =
+[
+    [ "openbridge_init", "group___open_bridge.html#ga473400ce44e727883998655e3c6bbec4", null ],
+    [ "openbridge_receive", "group___open_bridge.html#ga59de967f7924e1c672ada8905a54d683", null ],
+    [ "openbridge_sendDone", "group___open_bridge.html#ga48345d223f59957ed3b4b47ed692ef98", null ],
+    [ "openbridge_triggerData", "group___open_bridge.html#gab99c416c13d3966991ca89ec418f1acf", null ]
+];
---
+++ firmware/structidmanager__vars__t.js
@@ -0,0 +1,9 @@
+var structidmanager__vars__t =
+[
+    [ "isBridge", "structidmanager__vars__t.html#a018b91ffc2fd9cb7e2bab2790ca45d79", null ],
+    [ "isDAGroot", "structidmanager__vars__t.html#a0ad47ab43cefa963b2921da022e1658f", null ],
+    [ "my16bID", "structidmanager__vars__t.html#a3ddec815683ae0665f5919cd798cd3a8", null ],
+    [ "my64bID", "structidmanager__vars__t.html#a95fcc4bca1fd0b4f05fe13714bb65eb6", null ],
+    [ "myPANID", "structidmanager__vars__t.html#a6e395f63c8294c9147390ff717390af3", null ],
+    [ "myPrefix", "structidmanager__vars__t.html#a87338ea066a746cb0844ee367ed46c54", null ]
+];
