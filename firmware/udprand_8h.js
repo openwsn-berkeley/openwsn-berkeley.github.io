d793486fe1a540083676fd95b21b5c7155a77143
OpenWSN Builder <builder@openwsn.org>
2014-05-10 04:44:29 -0700

    removing old firmware documentation

--- firmware/udprand_8h.js
+++
@@ -1,9 +0,0 @@
-var udprand_8h =
-[
-    [ "udprand_debugPrint", "udprand_8h.html#gad4d54912ca201c79729b63d1d69041f9", null ],
-    [ "udprand_init", "udprand_8h.html#ga42f99b3c556f0a0f083a97decf96ff43", null ],
-    [ "udprand_receive", "udprand_8h.html#gac7d4ca31ed3b088ba59ebd1cd58c1d94", null ],
-    [ "udprand_sendDone", "udprand_8h.html#gaf1a0a6d3930b350883f953de08f001a4", null ],
-    [ "udprand_task", "udprand_8h.html#ga3b13714c3b8c6692a04f3ef3714c13d9", null ],
-    [ "udprand_trigger", "udprand_8h.html#ga1cd379bad86d80f18cb34ad0c43d8310", null ]
-];
