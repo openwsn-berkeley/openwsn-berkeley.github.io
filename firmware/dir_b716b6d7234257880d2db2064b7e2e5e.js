f5cf0907717265a092cec85705a8a671a88843d7
OpenWSN Builder <builder@openwsn.org>
2014-05-12 04:44:48 -0700

    adding new firmware documentation

---
+++ firmware/dir_b716b6d7234257880d2db2064b7e2e5e.js
@@ -0,0 +1,5 @@
+var dir_b716b6d7234257880d2db2064b7e2e5e =
+[
+    [ "tcpecho.c", "tcpecho_8c.html", "tcpecho_8c" ],
+    [ "tcpecho.h", "tcpecho_8h.html", "tcpecho_8h" ]
+];
---
+++ firmware/group__open_coap.js
@@ -0,0 +1,105 @@
+var group__open_coap =
+[
+    [ "coap_header_iht", "structcoap__header__iht.html", [
+      [ "Code", "structcoap__header__iht.html#aa72b8de1e63d9d5670bf65c7edbac73d", null ],
+      [ "messageID", "structcoap__header__iht.html#a48b8857a8afbd63a6aa95cea074e9ca5", null ],
+      [ "T", "structcoap__header__iht.html#a3af1fb0dc6e28d948139547984511081", null ],
+      [ "TKL", "structcoap__header__iht.html#ae729c461e1ed105c2f47d9a706fb2720", null ],
+      [ "token", "structcoap__header__iht.html#a914f56298e55ee7cd7e9e284467e1dfb", null ],
+      [ "Ver", "structcoap__header__iht.html#ab3044e7fa4830934d1b274d1bb2a2834", null ]
+    ] ],
+    [ "coap_option_iht", "structcoap__option__iht.html", [
+      [ "length", "structcoap__option__iht.html#ab2b3adeb2a67e656ff030b56727fd0ac", null ],
+      [ "pValue", "structcoap__option__iht.html#a9ddff936ddacfc2b6285295cb2ef2c9e", null ],
+      [ "type", "structcoap__option__iht.html#a3a7341d23a85b8b8bd3da2e7443ff63c", null ]
+    ] ],
+    [ "coap_resource_desc_t", "structcoap__resource__desc__t.html", [
+      [ "callbackRx", "structcoap__resource__desc__t.html#aeaa33d6840cfc1778fa2bdbdca204d8c", null ],
+      [ "callbackSendDone", "structcoap__resource__desc__t.html#a4d2a75d06295789331f439871f4190f3", null ],
+      [ "componentID", "structcoap__resource__desc__t.html#a4fdca17720603d2904382a8090fcd306", null ],
+      [ "messageID", "structcoap__resource__desc__t.html#a48b8857a8afbd63a6aa95cea074e9ca5", null ],
+      [ "next", "structcoap__resource__desc__t.html#a071de1f4b7cf41aa93e7d5c3cff98868", null ],
+      [ "path0len", "structcoap__resource__desc__t.html#af9a6e939da8557265cf0554bfee567bc", null ],
+      [ "path0val", "structcoap__resource__desc__t.html#a0fab9d3155ca9b3660d9566454c82b81", null ],
+      [ "path1len", "structcoap__resource__desc__t.html#ab5b1bde7b55b933140277d3726f83524", null ],
+      [ "path1val", "structcoap__resource__desc__t.html#a45769cd01822173f089b6c44f7035676", null ],
+      [ "token", "structcoap__resource__desc__t.html#afb6cdb8d697f6e4b925161d99a2daa3c", null ]
+    ] ],
+    [ "opencoap_vars_t", "structopencoap__vars__t.html", [
+      [ "busySending", "structopencoap__vars__t.html#adb63c747385c70bc79674b73f2a06dcc", null ],
+      [ "delayCounter", "structopencoap__vars__t.html#ac8f07bb3e6fd94b5ecb578fb006cd1cd", null ],
+      [ "messageID", "structopencoap__vars__t.html#a48b8857a8afbd63a6aa95cea074e9ca5", null ],
+      [ "resources", "structopencoap__vars__t.html#a8904a093f5c6302b8a1ea17d144fa1c3", null ]
+    ] ],
+    [ "COAP_MAX_TKL", "group__open_coap.html#gac74ba1d96b0defa127ca13247884a8d4", null ],
+    [ "COAP_PAYLOAD_MARKER", "group__open_coap.html#gab4e38910080deed0a5df4aec116d3999", null ],
+    [ "COAP_VERSION", "group__open_coap.html#ga0cc059ba01b6be3d8abe18cc96ab2221", null ],
+    [ "MAX_COAP_OPTIONS", "group__open_coap.html#ga3155afe0b07d32f0fb33faabc0ea0ec6", null ],
+    [ "callbackRx_cbt", "group__open_coap.html#gace5c3f8d6f51ae2090738909b834ee21", null ],
+    [ "callbackSendDone_cbt", "group__open_coap.html#gaaa5860881f8f33ef086a16329bb0c6b3", null ],
+    [ "coap_resource_desc_t", "group__open_coap.html#ga507ec6b3ff0f2c55b7965a3a243c4fd0", null ],
+    [ "coap_code_t", "group__open_coap.html#gae99fde61d59e68af70c6bcd0c7b0256f", [
+      [ "COAP_CODE_EMPTY", "group__open_coap.html#ggae99fde61d59e68af70c6bcd0c7b0256fad23d0c658ea18ce7e1dd8f1f4be0e7ec", null ],
+      [ "COAP_CODE_REQ_GET", "group__open_coap.html#ggae99fde61d59e68af70c6bcd0c7b0256faee813557f5b6ce3f2605ea60509447ee", null ],
+      [ "COAP_CODE_REQ_POST", "group__open_coap.html#ggae99fde61d59e68af70c6bcd0c7b0256faf2caaa1bc9e105a08052339a30a8c8a3", null ],
+      [ "COAP_CODE_REQ_PUT", "group__open_coap.html#ggae99fde61d59e68af70c6bcd0c7b0256fae340c702a79200fad8b4ae3dc207a4cb", null ],
+      [ "COAP_CODE_REQ_DELETE", "group__open_coap.html#ggae99fde61d59e68af70c6bcd0c7b0256fab58ccb57cab7604f22c4b0a8f869dd37", null ],
+      [ "COAP_CODE_RESP_CREATED", "group__open_coap.html#ggae99fde61d59e68af70c6bcd0c7b0256fa4df5a42ae3703e135f2c51cb9bece412", null ],
+      [ "COAP_CODE_RESP_DELETED", "group__open_coap.html#ggae99fde61d59e68af70c6bcd0c7b0256fa4c66de8dbed39024c4d24f079f419df0", null ],
+      [ "COAP_CODE_RESP_VALID", "group__open_coap.html#ggae99fde61d59e68af70c6bcd0c7b0256fa57f67a6e1de8273c6a59ae68e02fc844", null ],
+      [ "COAP_CODE_RESP_CHANGED", "group__open_coap.html#ggae99fde61d59e68af70c6bcd0c7b0256fac980e3068cfccc3a5b3c08fc847ed349", null ],
+      [ "COAP_CODE_RESP_CONTENT", "group__open_coap.html#ggae99fde61d59e68af70c6bcd0c7b0256fa27e8e136b607d81c8f9cbbcc892db046", null ],
+      [ "COAP_CODE_RESP_BADREQ", "group__open_coap.html#ggae99fde61d59e68af70c6bcd0c7b0256facf8fb9fbc56e2053ab4ada884bbd0c09", null ],
+      [ "COAP_CODE_RESP_UNAUTHORIZED", "group__open_coap.html#ggae99fde61d59e68af70c6bcd0c7b0256fa1e80eb762859e91ca40c6d150985069e", null ],
+      [ "COAP_CODE_RESP_BADOPTION", "group__open_coap.html#ggae99fde61d59e68af70c6bcd0c7b0256fa9486b10d11f22b898c7e8ef3595c2a4b", null ],
+      [ "COAP_CODE_RESP_FORBIDDEN", "group__open_coap.html#ggae99fde61d59e68af70c6bcd0c7b0256faeda5c2a149c5d007d7d881bcd368cc66", null ],
+      [ "COAP_CODE_RESP_NOTFOUND", "group__open_coap.html#ggae99fde61d59e68af70c6bcd0c7b0256fa8f34d21872bb3ab0339c9e2816a4c114", null ],
+      [ "COAP_CODE_RESP_METHODNOTALLOWED", "group__open_coap.html#ggae99fde61d59e68af70c6bcd0c7b0256fa7d6b3f39e38f2d69ab7820772570a67f", null ],
+      [ "COAP_CODE_RESP_PRECONDFAILED", "group__open_coap.html#ggae99fde61d59e68af70c6bcd0c7b0256fa640d3a42e249605e32c9b71d7d7d12cd", null ],
+      [ "COAP_CODE_RESP_REQTOOLARGE", "group__open_coap.html#ggae99fde61d59e68af70c6bcd0c7b0256fa07b49fa617733f4da4d503f01ad2283c", null ],
+      [ "COAP_CODE_RESP_UNSUPPMEDIATYPE", "group__open_coap.html#ggae99fde61d59e68af70c6bcd0c7b0256fa6eb39d897f3a73234b2bfa7c925ffeca", null ],
+      [ "COAP_CODE_RESP_SERVERERROR", "group__open_coap.html#ggae99fde61d59e68af70c6bcd0c7b0256fac1608678da628ec577acfcbc8f1e2bb0", null ],
+      [ "COAP_CODE_RESP_NOTIMPLEMENTED", "group__open_coap.html#ggae99fde61d59e68af70c6bcd0c7b0256fa667526f036e6721a1edd8deaf28c27bd", null ],
+      [ "COAP_CODE_RESP_BADGATEWAY", "group__open_coap.html#ggae99fde61d59e68af70c6bcd0c7b0256fac5f6381cb7c7351dfb5cca0ed6a5ab83", null ],
+      [ "COAP_CODE_RESP_UNAVAILABLE", "group__open_coap.html#ggae99fde61d59e68af70c6bcd0c7b0256fafe0932a9eccda4acd776bf29c89276b1", null ],
+      [ "COAP_CODE_RESP_GWTIMEOUT", "group__open_coap.html#ggae99fde61d59e68af70c6bcd0c7b0256fa8ff33be7084159f8fc5594e6f41e3740", null ],
+      [ "COAP_CODE_RESP_PROXYINGNOTSUPP", "group__open_coap.html#ggae99fde61d59e68af70c6bcd0c7b0256fa5acd962d666c451c0053b86555e39f09", null ]
+    ] ],
+    [ "coap_media_type_t", "group__open_coap.html#gaef18a213b721e5f11a0d12b394be1084", [
+      [ "COAP_MEDTYPE_TEXTPLAIN", "group__open_coap.html#ggaef18a213b721e5f11a0d12b394be1084a3eb620ed79f0fd008e1a099781f138e4", null ],
+      [ "COAP_MEDTYPE_APPLINKFORMAT", "group__open_coap.html#ggaef18a213b721e5f11a0d12b394be1084a4e096f618637348ab356c70524d1ddb9", null ],
+      [ "COAP_MEDTYPE_APPXML", "group__open_coap.html#ggaef18a213b721e5f11a0d12b394be1084ae086860d7a2cce52fafa75db92c08b92", null ],
+      [ "COAP_MEDTYPE_APPOCTETSTREAM", "group__open_coap.html#ggaef18a213b721e5f11a0d12b394be1084a0b140215aaa7e8763a81ff69218fef19", null ],
+      [ "COAP_MEDTYPE_APPEXI", "group__open_coap.html#ggaef18a213b721e5f11a0d12b394be1084ad5bae428b527387f8a7b329a6283f8e5", null ],
+      [ "COAP_MEDTYPE_APPJSON", "group__open_coap.html#ggaef18a213b721e5f11a0d12b394be1084acb5118bf020133735067e07700b57c15", null ]
+    ] ],
+    [ "coap_option_t", "group__open_coap.html#ga809c9a7522aa7e4726b4a95ccdb2833b", [
+      [ "COAP_OPTION_NONE", "group__open_coap.html#gga809c9a7522aa7e4726b4a95ccdb2833ba6f1c9871b73e1ce158e1f671393cdcdc", null ],
+      [ "COAP_OPTION_NUM_IFMATCH", "group__open_coap.html#gga809c9a7522aa7e4726b4a95ccdb2833ba9f7901f5b91c312b84adfa610ffdfce7", null ],
+      [ "COAP_OPTION_NUM_URIHOST", "group__open_coap.html#gga809c9a7522aa7e4726b4a95ccdb2833baa8389f3015218b9b418e89d1214f18a7", null ],
+      [ "COAP_OPTION_NUM_ETAG", "group__open_coap.html#gga809c9a7522aa7e4726b4a95ccdb2833ba9d495f2b02838985081c1e470a04feb2", null ],
+      [ "COAP_OPTION_NUM_IFNONEMATCH", "group__open_coap.html#gga809c9a7522aa7e4726b4a95ccdb2833baa0efddd89894e9d5499fb6104ade8181", null ],
+      [ "COAP_OPTION_NUM_URIPORT", "group__open_coap.html#gga809c9a7522aa7e4726b4a95ccdb2833ba6471be06a48e3bffb30274052ee3381c", null ],
+      [ "COAP_OPTION_NUM_LOCATIONPATH", "group__open_coap.html#gga809c9a7522aa7e4726b4a95ccdb2833ba4ee718814051f694c7ae236965c6536f", null ],
+      [ "COAP_OPTION_NUM_URIPATH", "group__open_coap.html#gga809c9a7522aa7e4726b4a95ccdb2833ba38d8e0fc51d8055261081086cf0691cb", null ],
+      [ "COAP_OPTION_NUM_CONTENTFORMAT", "group__open_coap.html#gga809c9a7522aa7e4726b4a95ccdb2833bae59c8e513355b8fa14ea7149971f3a00", null ],
+      [ "COAP_OPTION_NUM_MAXAGE", "group__open_coap.html#gga809c9a7522aa7e4726b4a95ccdb2833ba5da32446e8a58275277ec87556c1d457", null ],
+      [ "COAP_OPTION_NUM_URIQUERY", "group__open_coap.html#gga809c9a7522aa7e4726b4a95ccdb2833bab3039cd2da76c4a490307b9f56e262fa", null ],
+      [ "COAP_OPTION_NUM_ACCEPT", "group__open_coap.html#gga809c9a7522aa7e4726b4a95ccdb2833ba68083aabd42f07eff26a8f437b73b399", null ],
+      [ "COAP_OPTION_NUM_LOCATIONQUERY", "group__open_coap.html#gga809c9a7522aa7e4726b4a95ccdb2833ba0588bac7e805aaac1bb1eec2094cf49a", null ],
+      [ "COAP_OPTION_NUM_PROXYURI", "group__open_coap.html#gga809c9a7522aa7e4726b4a95ccdb2833ba4332c205fc8ebbe6f3a946c2bd8e00a3", null ],
+      [ "COAP_OPTION_NUM_PROXYSCHEME", "group__open_coap.html#gga809c9a7522aa7e4726b4a95ccdb2833ba8a4439e4549f41301a471c6990bf76b9", null ]
+    ] ],
+    [ "coap_type_t", "group__open_coap.html#gafb608e0d646391b17d00513242200087", [
+      [ "COAP_TYPE_CON", "group__open_coap.html#ggafb608e0d646391b17d00513242200087a65c04ee4847d0c595238079ac9564e8d", null ],
+      [ "COAP_TYPE_NON", "group__open_coap.html#ggafb608e0d646391b17d00513242200087a26b4b8fe681ea0720d68e1711741e073", null ],
+      [ "COAP_TYPE_ACK", "group__open_coap.html#ggafb608e0d646391b17d00513242200087a7b2fe2187018bce9132af2763b57307d", null ],
+      [ "COAP_TYPE_RES", "group__open_coap.html#ggafb608e0d646391b17d00513242200087a924098f5424116957c8cf549c50ff066", null ]
+    ] ],
+    [ "opencoap_init", "group__open_coap.html#ga326bc26218ba65f2accb56cb1a1019d5", null ],
+    [ "opencoap_receive", "group__open_coap.html#gaaa4778341676139ae72ad5dd515a816d", null ],
+    [ "opencoap_register", "group__open_coap.html#ga82e1d3e8b96366e52348c479f7582c0d", null ],
+    [ "opencoap_send", "group__open_coap.html#gaa4a1f14228086a934a38230eb0254323", null ],
+    [ "opencoap_sendDone", "group__open_coap.html#gad576f6262590140be3771817a151fc45", null ],
+    [ "opencoap_writeLinks", "group__open_coap.html#ga7ae19f5ee533a4000f76e5c74b087c73", null ]
+];
