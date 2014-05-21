818a0ccca2c8f964366defbc28256591be06a190
OpenWSN Builder <builder@openwsn.org>
2014-05-21 04:46:10 -0700

    removing old firmware documentation

--- firmware/openudp_8c.js
+++
@@ -1,8 +0,0 @@
-var openudp_8c =
-[
-    [ "openudp_debugPrint", "openudp_8c.html#gae8b9c040bb071f20664217fd7f337632", null ],
-    [ "openudp_init", "openudp_8c.html#gaa3b0758b70896d18057e2fc96cc651bb", null ],
-    [ "openudp_receive", "openudp_8c.html#gadeef7f3934e35ea47f1869a27252ac93", null ],
-    [ "openudp_send", "openudp_8c.html#ga9e3ddc36106b92231cf7cfb3b5cabbd4", null ],
-    [ "openudp_sendDone", "openudp_8c.html#ga30228c5d4c8404ec4125ca154badbe71", null ]
-];
