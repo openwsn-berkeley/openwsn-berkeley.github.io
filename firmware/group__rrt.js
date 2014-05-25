5ed07a2c1472c1c36468f90296801c866ad1775b
OpenWSN Builder <builder@openwsn.org>
2014-05-25 04:46:26 -0700

    removing old firmware documentation

--- firmware/group__rrt.js
+++
@@ -1,7 +0,0 @@
-var group__rrt =
-[
-    [ "rrt_vars_t", "structrrt__vars__t.html", [
-      [ "desc", "structrrt__vars__t.html#af824c41339fef8ae82ca38c2ddc4eab8", null ]
-    ] ],
-    [ "rrt_init", "group__rrt.html#gac43e94e8af974a4c334a41ecc1640576", null ]
-];
--- firmware/structrrt__vars__t.js
+++
@@ -1,4 +0,0 @@
-var structrrt__vars__t =
-[
-    [ "desc", "structrrt__vars__t.html#af824c41339fef8ae82ca38c2ddc4eab8", null ]
-];
