df1ed4287ef6e4bab8bab98ec1428c81624e50c0
OpenWSN Builder <builder@openwsn.org>
2014-05-30 04:45:54 -0700

    adding new firmware documentation

---
+++ firmware/group__radiotimer.js
@@ -0,0 +1,18 @@
+var group__radiotimer =
+[
+    [ "radiotimer_capture_cbt", "group__radiotimer.html#gab80fe8aced5508af758f508c33ed6709", null ],
+    [ "radiotimer_compare_cbt", "group__radiotimer.html#gaddbc13b181caf1c82d480211788454b6", null ],
+    [ "radiotimer_cancel", "group__radiotimer.html#gafed4b9b06bccad092a40cfa2f9a8f9a4", null ],
+    [ "radiotimer_getCapturedTime", "group__radiotimer.html#ga0f3e5327177b7e0286b103de3c855650", null ],
+    [ "radiotimer_getPeriod", "group__radiotimer.html#gae69daba372a0617c1de097b15fd34cf0", null ],
+    [ "radiotimer_getValue", "group__radiotimer.html#ga1c7e3e384d4e460dba6bd7caf4194a4a", null ],
+    [ "radiotimer_init", "group__radiotimer.html#gad403aae0de5b576d042ad60e61f07759", null ],
+    [ "radiotimer_isr", "group__radiotimer.html#gafaa9c720b9ebce9e278c7d61bdfd545e", null ],
+    [ "radiotimer_schedule", "group__radiotimer.html#ga137a48c413a18f689b328b2be259704c", null ],
+    [ "radiotimer_setCompareCb", "group__radiotimer.html#gaf4d7c5df5b6c28462c5104df496ae584", null ],
+    [ "radiotimer_setEndFrameCb", "group__radiotimer.html#ga54531cf0f173f07f760d24aeacbc6fe3", null ],
+    [ "radiotimer_setOverflowCb", "group__radiotimer.html#gab892e556a83f22551ff4e4551bd488f7", null ],
+    [ "radiotimer_setPeriod", "group__radiotimer.html#gaa976e43c1c475e9ff23b26ed06fa6918", null ],
+    [ "radiotimer_setStartFrameCb", "group__radiotimer.html#gad6bae4a356aa6856a48dd247eea43f84", null ],
+    [ "radiotimer_start", "group__radiotimer.html#ga5afd5ed18d32fb118dca4ea99d181e35", null ]
+];
