a4d9ceeb3b4edba80dc4b0092ea35f70a2617834
OpenWSN Builder <builder@openwsn.org>
2014-05-31 04:45:37 -0700

    removing old firmware documentation

--- firmware/structsynch___i_e__t.js
+++
@@ -1,5 +0,0 @@
-var structsynch___i_e__t =
-[
-    [ "asn", "structsynch___i_e__t.html#abdecca20a05b214c5fef4d57521a31fb", null ],
-    [ "join_priority", "structsynch___i_e__t.html#a2bd90b1232b9c642a3d6f9d9ee338d99", null ]
-];
