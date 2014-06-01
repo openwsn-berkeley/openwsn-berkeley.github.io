e3be8097e6245f1212e5452e05c0ea0b6583739b
OpenWSN Builder <builder@openwsn.org>
2014-06-01 04:46:07 -0700

    removing old firmware documentation

--- firmware/structdebug_open_queue_entry__t.js
+++
@@ -1,5 +0,0 @@
-var structdebug_open_queue_entry__t =
-[
-    [ "creator", "structdebug_open_queue_entry__t.html#afc5fe098028e4d27c254aef961d54857", null ],
-    [ "owner", "structdebug_open_queue_entry__t.html#a1c3b8d8797515eea0e86cbdb2d85aed6", null ]
-];
