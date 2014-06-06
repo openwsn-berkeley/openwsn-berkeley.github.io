7aae3dfede160ba38896e322b4ecbafe41ac2674
OpenWSN Builder <builder@openwsn.org>
2014-06-06 12:16:56 -0700

    adding new firmware documentation

---
+++ firmware/adaptive__sync_8h.js
@@ -0,0 +1,14 @@
+var adaptive__sync_8h =
+[
+    [ "adaptive_sync_state_t", "adaptive__sync_8h.html#gac81060fb836128f6649b1249f2eca999", [
+      [ "S_NONE", "adaptive__sync_8h.html#ggac81060fb836128f6649b1249f2eca999acb5ec123e88fa0c9a1548d1f05b03e0f", null ],
+      [ "S_FASTER", "adaptive__sync_8h.html#ggac81060fb836128f6649b1249f2eca999a04968eb2fb707873f1ef525cc2dcdaca", null ],
+      [ "S_SLOWER", "adaptive__sync_8h.html#ggac81060fb836128f6649b1249f2eca999a9687b49b1490ae284f1be634573de4ee", null ]
+    ] ],
+    [ "adaptive_sync_calculateCompensatedSlots", "adaptive__sync_8h.html#ga4754d7eb03173c9d65256a04abbdd3a1", null ],
+    [ "adaptive_sync_countCompensationTimeout", "adaptive__sync_8h.html#gacf9f3f07c3daf577f34b5e80584dc25f", null ],
+    [ "adaptive_sync_countCompensationTimeout_compoundSlots", "adaptive__sync_8h.html#gadfc86247876d1706bb652832d8dd4cc5", null ],
+    [ "adaptive_sync_driftChanged", "adaptive__sync_8h.html#gadff0464cf68a29739ec724c7be9d311e", null ],
+    [ "adaptive_sync_init", "adaptive__sync_8h.html#gaef3ef6115193e6519adf836f433e77c8", null ],
+    [ "adaptive_sync_preprocess", "adaptive__sync_8h.html#ga8425186666bdbb057c06492731509d60", null ]
+];
---
+++ firmware/dir_28eadef165966256a4eac12f12e92df7.js
@@ -0,0 +1,5 @@
+var dir_28eadef165966256a4eac12f12e92df7 =
+[
+    [ "r6t.c", "r6t_8c.html", "r6t_8c" ],
+    [ "r6t.h", "r6t_8h.html", "r6t_8h" ]
+];
