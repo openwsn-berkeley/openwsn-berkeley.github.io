17ff37c324c03a5bd6d5621c3e6ccbf21fe5b97d
OpenWSN Builder <builder@openwsn.org>
2014-06-16 04:46:27 -0700

    removing old firmware documentation

--- firmware/rleds_8h.js
+++
@@ -1,4 +0,0 @@
-var rleds_8h =
-[
-    [ "rleds__init", "rleds_8h.html#gaeff5d638d3ff076402569577d1c0666f", null ]
-];
--- firmware/structimu__vars__t.js
+++
@@ -1,5 +0,0 @@
-var structimu__vars__t =
-[
-    [ "mesurements_left", "structimu__vars__t.html#ad5cca281343de922988fd56b54a67aaf", null ],
-    [ "pktReceived", "structimu__vars__t.html#af289bb270589de96d25328cdb4383617", null ]
-];
