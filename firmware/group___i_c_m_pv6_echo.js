a043c502c20335e040a0592f2bed6256094fab99
OpenWSN Builder <builder@openwsn.org>
2014-06-18 04:46:53 -0700

    adding new firmware documentation

---
+++ firmware/group___i_c_m_pv6_echo.js
@@ -0,0 +1,12 @@
+var group___i_c_m_pv6_echo =
+[
+    [ "icmpv6echo_vars_t", "structicmpv6echo__vars__t.html", [
+      [ "busySending", "structicmpv6echo__vars__t.html#adb63c747385c70bc79674b73f2a06dcc", null ],
+      [ "hisAddress", "structicmpv6echo__vars__t.html#a5878b2b3978e0ceee14715db652dd5ad", null ],
+      [ "seq", "structicmpv6echo__vars__t.html#a0a97914b75ed3ab7d0e13d8b200d758a", null ]
+    ] ],
+    [ "icmpv6echo_init", "group___i_c_m_pv6_echo.html#gaa18a0f1c239132fc754e43f475b33995", null ],
+    [ "icmpv6echo_receive", "group___i_c_m_pv6_echo.html#gaa78a139f3dd544c9a78e5c7397419408", null ],
+    [ "icmpv6echo_sendDone", "group___i_c_m_pv6_echo.html#ga0502bcb8a36b276660fd1b4b40939753", null ],
+    [ "icmpv6echo_trigger", "group___i_c_m_pv6_echo.html#ga9ac3ae0e9d1e46044070298bc24fee82", null ]
+];
---
+++ firmware/openrandom_8c.js
@@ -0,0 +1,6 @@
+var openrandom_8c =
+[
+    [ "openrandom_get16b", "openrandom_8c.html#ga70626e5a23d14d7d426517fb5389252d", null ],
+    [ "openrandom_init", "openrandom_8c.html#gaba64bed43b28e866086767c9daf83390", null ],
+    [ "random_vars", "openrandom_8c.html#ae436857566b6ad035ea13f70248fa740", null ]
+];
