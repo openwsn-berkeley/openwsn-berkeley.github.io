e6823bd3e64ee972a470153c16008f3f1f42154f
OpenWSN Builder <builder@openwsn.org>
2014-06-21 04:46:15 -0700

    adding new firmware documentation

---
+++ firmware/openqueue_8c.js
@@ -0,0 +1,15 @@
+var openqueue_8c =
+[
+    [ "debugPrint_queue", "openqueue_8c.html#ga38fcfaa1cfdce93705865854766bc282", null ],
+    [ "openqueue_freePacketBuffer", "openqueue_8c.html#ga326f90b6a0c7a331257b524a5af236d4", null ],
+    [ "openqueue_getFreePacketBuffer", "openqueue_8c.html#ga40baa99cf50015c0f478b699c54ad211", null ],
+    [ "openqueue_init", "openqueue_8c.html#ga9ff14ef9d45f660f007b597d5954724e", null ],
+    [ "openqueue_macGetAdvPacket", "openqueue_8c.html#ga20606ec8017194b87cb747a01c4c00a1", null ],
+    [ "openqueue_macGetDataPacket", "openqueue_8c.html#ga654ff57cc43c12704d87dc66fcfe7fa2", null ],
+    [ "openqueue_removeAllCreatedBy", "openqueue_8c.html#gade7d9553bb4ff27f1285bbc4905d7831", null ],
+    [ "openqueue_removeAllOwnedBy", "openqueue_8c.html#gab4a47e0afad6ca760d6dbef15787ee10", null ],
+    [ "openqueue_reset_entry", "openqueue_8c.html#a8d632c3b154b12057f7c18e08da6648b", null ],
+    [ "openqueue_resGetReceivedPacket", "openqueue_8c.html#gabf5dd7275ed68794bca47b35f5ab9726", null ],
+    [ "openqueue_resGetSentPacket", "openqueue_8c.html#ga9cea2b07856b14bc6226df8fe3a962dc", null ],
+    [ "openqueue_vars", "openqueue_8c.html#ac5a185e8387d512bb97280c54c68c264", null ]
+];
---
+++ firmware/r6t_8h.js
@@ -0,0 +1,13 @@
+var r6t_8h =
+[
+    [ "R6T_MAXRESPONSES", "r6t_8h.html#ga5b9c0de617e5e7335150b010ef99e088", null ],
+    [ "link_command_t", "r6t_8h.html#ga29ad3bb2c43da0369b63aa4a766e9d0d", [
+      [ "CREATE_LINK", "r6t_8h.html#gga29ad3bb2c43da0369b63aa4a766e9d0da5e0f7d31cbd01a75d5e8f245bcea2ab4", null ],
+      [ "READ_LINK", "r6t_8h.html#gga29ad3bb2c43da0369b63aa4a766e9d0da303b831897d9321cfe94c5d070d4f0ad", null ],
+      [ "UPDATE_LINK", "r6t_8h.html#gga29ad3bb2c43da0369b63aa4a766e9d0dadd6e3224538ac3b246c456965b51c972", null ],
+      [ "DELETE_LINK", "r6t_8h.html#gga29ad3bb2c43da0369b63aa4a766e9d0dab3419d0e441c04356de2e180aa13479f", null ]
+    ] ],
+    [ "PRAGMA", "r6t_8h.html#ga44f3d4b22a86e1e2a6dbc81bd4c92be1", null ],
+    [ "PRAGMA", "r6t_8h.html#ga383707c8cf57a58ab67888eb545034dd", null ],
+    [ "r6t_init", "r6t_8h.html#ga014edd7aedf5bf0e426740e28da28ba9", null ]
+];
