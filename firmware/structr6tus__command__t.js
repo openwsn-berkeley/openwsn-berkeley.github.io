718671c40bfda5c5f33cb2b977723cd8beac928d
OpenWSN Builder <builder@openwsn.org>
2014-06-25 04:47:28 -0700

    adding new firmware documentation

---
+++ firmware/structr6tus__command__t.js
@@ -0,0 +1,5 @@
+var structr6tus__command__t =
+[
+    [ "numelem", "structr6tus__command__t.html#a1379013763dbfc501f73904fbb425810", null ],
+    [ "type", "structr6tus__command__t.html#a4a120fb1b6aafa3f205ddba5b37d4509", null ]
+];
