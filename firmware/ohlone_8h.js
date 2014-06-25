be183b8a806bc648cd76b36137f4138fd52f0378
OpenWSN Builder <builder@openwsn.org>
2014-06-25 04:47:02 -0700

    removing old firmware documentation

--- firmware/ohlone_8h.js
+++
@@ -1,9 +0,0 @@
-var ohlone_8h =
-[
-    [ "ohlone_connectDone", "ohlone_8h.html#ga34e022fe72c08d29a6374d122e6df689", null ],
-    [ "ohlone_debugPrint", "ohlone_8h.html#ga9db9e234a438966242cc4753de341ad8", null ],
-    [ "ohlone_init", "ohlone_8h.html#gaaf3602cc06577d1f448df02cf14e03f0", null ],
-    [ "ohlone_receive", "ohlone_8h.html#ga3f0d74cc087dd235fb3b90b1e11e923d", null ],
-    [ "ohlone_sendDone", "ohlone_8h.html#gab474bbb43cc1a9384e7885dda5255f19", null ],
-    [ "ohlone_shouldIlisten", "ohlone_8h.html#ga6f68488319d6ff04108da5da53549da5", null ]
-];
