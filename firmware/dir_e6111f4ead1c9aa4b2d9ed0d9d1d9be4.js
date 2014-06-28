0d8943e2bc3f8b74a4e34237545ee580312bc434
OpenWSN Builder <builder@openwsn.org>
2014-06-28 04:47:50 -0700

    adding new firmware documentation

---
+++ firmware/dir_e6111f4ead1c9aa4b2d9ed0d9d1d9be4.js
@@ -0,0 +1,5 @@
+var dir_e6111f4ead1c9aa4b2d9ed0d9d1d9be4 =
+[
+    [ "udplatency.c", "udplatency_8c.html", "udplatency_8c" ],
+    [ "udplatency.h", "udplatency_8h.html", "udplatency_8h" ]
+];
---
+++ firmware/group___neighbors.js
@@ -0,0 +1,63 @@
+var group___neighbors =
+[
+    [ "neighborRow_t", "structneighbor_row__t.html", [
+      [ "addr_64b", "structneighbor_row__t.html#a0c5a1796891070944ae492fb732017c7", null ],
+      [ "asn", "structneighbor_row__t.html#ae0d42a38f9f6a63cda898e7971df742e", null ],
+      [ "DAGrank", "structneighbor_row__t.html#a1b9fd98f0625d88f4dfd23af7ecf590f", null ],
+      [ "joinPrio", "structneighbor_row__t.html#a2d0606b98bb6be3aa5b934654a2c68b1", null ],
+      [ "numRx", "structneighbor_row__t.html#ab633a48aed6e6248411c9cb4b4d309cc", null ],
+      [ "numTx", "structneighbor_row__t.html#a80785a5e701e393a549c13704aeb4905", null ],
+      [ "numTxACK", "structneighbor_row__t.html#a86bffffaee99f71b951a0f8ee7574c7a", null ],
+      [ "numWraps", "structneighbor_row__t.html#ac61321d7ff132bd409faccd66cbabad0", null ],
+      [ "parentPreference", "structneighbor_row__t.html#ad2c1a394ed19cf899e54c2a2867fc3d0", null ],
+      [ "rssi", "structneighbor_row__t.html#a3b962e67ba74725bd60ca3c29f785abe", null ],
+      [ "stableNeighbor", "structneighbor_row__t.html#a441047d629ba5f4fa3e36c20925c0dce", null ],
+      [ "switchStabilityCounter", "structneighbor_row__t.html#af7983be2e9ec469b8588c0a89b536141", null ],
+      [ "used", "structneighbor_row__t.html#accd366cbfd7b8a1b904ca4177a894f95", null ]
+    ] ],
+    [ "debugNeighborEntry_t", "structdebug_neighbor_entry__t.html", [
+      [ "neighborEntry", "structdebug_neighbor_entry__t.html#aaa5329c5938449b437f3c0c1c3e95319", null ],
+      [ "row", "structdebug_neighbor_entry__t.html#ab86a0a249da45b017b7cb9bbb6982177", null ]
+    ] ],
+    [ "netDebugNeigborEntry_t", "structnet_debug_neigbor_entry__t.html", [
+      [ "asn", "structnet_debug_neigbor_entry__t.html#ac2d02a476c07bbe8c27d7c2c2718a896", null ],
+      [ "DAGrank", "structnet_debug_neigbor_entry__t.html#a1b9fd98f0625d88f4dfd23af7ecf590f", null ],
+      [ "last_addr_byte", "structnet_debug_neigbor_entry__t.html#ac34b98cafa2d6e0eb73d4847f0712823", null ],
+      [ "parentPreference", "structnet_debug_neigbor_entry__t.html#ad2c1a394ed19cf899e54c2a2867fc3d0", null ],
+      [ "rssi", "structnet_debug_neigbor_entry__t.html#a3b962e67ba74725bd60ca3c29f785abe", null ]
+    ] ],
+    [ "neighbors_vars_t", "structneighbors__vars__t.html", [
+      [ "debugRow", "structneighbors__vars__t.html#af07afa06893447b8a734f4b1d4d19dda", null ],
+      [ "dio", "structneighbors__vars__t.html#a845475c4dddf9bbb79d41c6a9c403bd4", null ],
+      [ "myDAGrank", "structneighbors__vars__t.html#abc176446e4ce7e3da9ff0fad5038f598", null ],
+      [ "neighbors", "structneighbors__vars__t.html#ae791cd36b10b458a91bb51dfade54a9f", null ]
+    ] ],
+    [ "BADNEIGHBORMAXRSSI", "group___neighbors.html#ga2c403faa9006b83e020166536b480025", null ],
+    [ "DEFAULTDAGRANK", "group___neighbors.html#gacc9a3232f2dca59348d8b536005a2f89", null ],
+    [ "DEFAULTLINKCOST", "group___neighbors.html#ga4d2874b0e44346069a08e6cf15d85d87", null ],
+    [ "GOODNEIGHBORMINRSSI", "group___neighbors.html#ga6e36748acedcb5b5ca037ad19227aeec", null ],
+    [ "MAXDAGRANK", "group___neighbors.html#gaec4751ff896721a5a0fd70b841d09724", null ],
+    [ "MAXNUMNEIGHBORS", "group___neighbors.html#gaf83606573bb704d34705b540c226a0d9", null ],
+    [ "MAXPREFERENCE", "group___neighbors.html#ga6a269e3fd9f5dc2fe01c351944352799", null ],
+    [ "MINHOPRANKINCREASE", "group___neighbors.html#gaa81003e9317e9cd007be68d37a09eb40", null ],
+    [ "SWITCHSTABILITYTHRESHOLD", "group___neighbors.html#ga756051d437b8e4198fe0432b5d73a48b", null ],
+    [ "debugNetPrint_neighbors", "group___neighbors.html#gaa49275db2066ad4430e2fa000e97c8b3", null ],
+    [ "debugPrint_neighbors", "group___neighbors.html#ga85475c492d943f16dc6da4fea6683352", null ],
+    [ "neighbors_getKANeighbor", "group___neighbors.html#gae741527ad75fcd31a5d7d6fc585174b2", null ],
+    [ "neighbors_getMyDAGrank", "group___neighbors.html#gab749e7566b80148cd38495cd7bd62272", null ],
+    [ "neighbors_getNeighbor", "group___neighbors.html#ga1c1819e3a38fb549a91c495d658b260f", null ],
+    [ "neighbors_getNumNeighbors", "group___neighbors.html#ga07d5dcae3e882a8a9a85997136952fcc", null ],
+    [ "neighbors_getPreferredParentEui64", "group___neighbors.html#gaaa20cedbb0eb4d6fb9389ccb211a3903", null ],
+    [ "neighbors_indicateRx", "group___neighbors.html#gaf6850dedabd52cdef3690aee1b1c554c", null ],
+    [ "neighbors_indicateRxDIO", "group___neighbors.html#gaa4e24acdf2f814068990c3e535a65164", null ],
+    [ "neighbors_indicateTx", "group___neighbors.html#ga8379ad6d1816deed995c75033ffd740a", null ],
+    [ "neighbors_init", "group___neighbors.html#ga4e9c73e9e7ec8a90679bed5d18b593a7", null ],
+    [ "neighbors_isNeighborWithHigherDAGrank", "group___neighbors.html#gac13c1a5dda0cc06109b811f093b1a400", null ],
+    [ "neighbors_isNeighborWithLowerDAGrank", "group___neighbors.html#ga3a84fbff4104444fdab844d9cbaa0c77", null ],
+    [ "neighbors_isPreferredParent", "group___neighbors.html#ga6811829f946d51d6e30b2f9f812df133", null ],
+    [ "neighbors_isStableNeighbor", "group___neighbors.html#gada9893dd745640061b063a06c67be154", null ],
+    [ "neighbors_removeOld", "group___neighbors.html#ga3fa32881f8cf7afa1e5334cef8185957", null ],
+    [ "neighbors_updateMyDAGrankAndNeighborPreference", "group___neighbors.html#ga995345b6dec99e3e20ef4059ed59db34", null ],
+    [ "BEGIN_PACK", "group___neighbors.html#ga89eba151bcc637ac64e49664e13a63ac", null ],
+    [ "END_PACK", "group___neighbors.html#ga662d0a92293349b73ee1fe0a480a6fbf", null ]
+];
