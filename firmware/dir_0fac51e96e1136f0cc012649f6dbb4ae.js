c71735073a98d1670e58e2b4d135b5387c2b9024
OpenWSN Builder <builder@openwsn.org>
2014-06-29 04:47:41 -0700

    removing old firmware documentation

--- firmware/dir_0fac51e96e1136f0cc012649f6dbb4ae.js
+++
@@ -1,5 +0,0 @@
-var dir_0fac51e96e1136f0cc012649f6dbb4ae =
-[
-    [ "swarmband.c", "swarmband_8c.html", null ],
-    [ "swarmband.h", "swarmband_8h.html", null ]
-];
--- firmware/group__rinfo.js
+++
@@ -1,7 +0,0 @@
-var group__rinfo =
-[
-    [ "rinfo_vars_t", "structrinfo__vars__t.html", [
-      [ "desc", "structrinfo__vars__t.html#af824c41339fef8ae82ca38c2ddc4eab8", null ]
-    ] ],
-    [ "rinfo_init", "group__rinfo.html#gaf7cf26c7f133967fc1cfe5c7c5168f85", null ]
-];
