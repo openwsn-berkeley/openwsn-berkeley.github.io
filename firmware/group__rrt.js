c92f3ef028755576576785f1e8ec2f6f699d1f06
OpenWSN Builder <builder@openwsn.org>
2014-07-06 04:47:38 -0700

    removing old firmware documentation

--- firmware/group__rrt.js
+++
@@ -1,7 +0,0 @@
-var group__rrt =
-[
-    [ "rrt_vars_t", "structrrt__vars__t.html", [
-      [ "desc", "structrrt__vars__t.html#af824c41339fef8ae82ca38c2ddc4eab8", null ]
-    ] ],
-    [ "rrt_init", "group__rrt.html#ga4175f20cafc20669a11649e60e46455e", null ]
-];
--- firmware/scheduler_8c.js
+++
@@ -1,9 +0,0 @@
-var scheduler_8c =
-[
-    [ "consumeTask", "scheduler_8c.html#a05491cc55a1d82da41a834a919f2a8d6", null ],
-    [ "scheduler_init", "scheduler_8c.html#gab258cc2856e5f6bb125907cc23358fbd", null ],
-    [ "scheduler_push_task", "scheduler_8c.html#ga5613eda7e7cabb9412e590b8eb7fa778", null ],
-    [ "scheduler_start", "scheduler_8c.html#ga6229a629147366d0af22ca2228612dc8", null ],
-    [ "scheduler_dbg", "scheduler_8c.html#a2651c472c49e62b94206de22b7b9d7ae", null ],
-    [ "scheduler_vars", "scheduler_8c.html#ad9c283c8f95de33ffee63e8a763811c8", null ]
-];
