f1433db20461a8704462730af4af91bb3fed16bb
OpenWSN Builder <builder@openwsn.org>
2014-07-11 04:46:39 -0700

    adding new firmware documentation

---
+++ firmware/group__uart.js
@@ -0,0 +1,19 @@
+var group__uart =
+[
+    [ "uart_rx_cbt", "group__uart.html#ga19b8c14fbf337dede332e30ed11d2394", null ],
+    [ "uart_tx_cbt", "group__uart.html#ga726c0730ef74d599b0723d0c2ee42cf5", null ],
+    [ "uart_event_t", "group__uart.html#gaf739f04f788018b4b59fb7ad75da0a8d", [
+      [ "UART_EVENT_THRES", "group__uart.html#ggaf739f04f788018b4b59fb7ad75da0a8dad4e17a7ecd06bd3b3af002804023f4c7", null ],
+      [ "UART_EVENT_OVERFLOW", "group__uart.html#ggaf739f04f788018b4b59fb7ad75da0a8da3b7ab5094830f3a016e40056fe8d0ad0", null ]
+    ] ],
+    [ "uart_clearRxInterrupts", "group__uart.html#gad4ce3cb232f8a2a473976527d3497fc6", null ],
+    [ "uart_clearTxInterrupts", "group__uart.html#gae5f8c18830d4a12c2ccd5aaab95aa563", null ],
+    [ "uart_disableInterrupts", "group__uart.html#ga138d10923b5872cc48a30802f9a85308", null ],
+    [ "uart_enableInterrupts", "group__uart.html#ga19d6ac545de3750804fcdd7d5c6ae41e", null ],
+    [ "uart_init", "group__uart.html#ga0c0ca72359ddf28dcd15900dfba19343", null ],
+    [ "uart_readByte", "group__uart.html#ga9b9f9e22d7f34dd0a2fe9c3456d12987", null ],
+    [ "uart_rx_isr", "group__uart.html#ga0c9173d92e6ad1afee36bf6face8d92e", null ],
+    [ "uart_setCallbacks", "group__uart.html#ga9a390382a1fb16e9d83c1f5a34afb6e3", null ],
+    [ "uart_tx_isr", "group__uart.html#ga20f850e163363a82ca205c9fdba31860", null ],
+    [ "uart_writeByte", "group__uart.html#ga7a4111d6f4c172637301fe00e344e77f", null ]
+];
