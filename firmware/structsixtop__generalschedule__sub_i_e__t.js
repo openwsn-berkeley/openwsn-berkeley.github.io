45907eed4632287fbf476516834212ee99d64117
OpenWSN Builder <builder@openwsn.org>
2014-07-15 04:46:46 -0700

    removing old firmware documentation

--- firmware/structsixtop__generalschedule__sub_i_e__t.js
+++
@@ -1,9 +0,0 @@
-var structsixtop__generalschedule__sub_i_e__t =
-[
-    [ "flag", "structsixtop__generalschedule__sub_i_e__t.html#a8b3ab54ed3e81c69863d65e4e6c424a0", null ],
-    [ "frameID", "structsixtop__generalschedule__sub_i_e__t.html#a2d84c5352abe58cba736bc8a3982ec4d", null ],
-    [ "length", "structsixtop__generalschedule__sub_i_e__t.html#ab2b3adeb2a67e656ff030b56727fd0ac", null ],
-    [ "linklist", "structsixtop__generalschedule__sub_i_e__t.html#acdadc392683f37532936142cd4d174e8", null ],
-    [ "numberOfcells", "structsixtop__generalschedule__sub_i_e__t.html#a4f080e28a758418d376e68199c59ec2a", null ],
-    [ "type", "structsixtop__generalschedule__sub_i_e__t.html#a1d127017fb298b889f4ba24752d08b8e", null ]
-];
