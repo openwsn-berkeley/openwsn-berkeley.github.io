31d074624eab8fcfb58ab3c0803b82f72d078edb
OpenWSN Builder <builder@openwsn.org>
2014-07-14 21:52:05 -0700

    removing old firmware documentation

--- firmware/structopencoap__vars__t.js
+++
@@ -1,7 +0,0 @@
-var structopencoap__vars__t =
-[
-    [ "busySending", "structopencoap__vars__t.html#adb63c747385c70bc79674b73f2a06dcc", null ],
-    [ "delayCounter", "structopencoap__vars__t.html#ac8f07bb3e6fd94b5ecb578fb006cd1cd", null ],
-    [ "messageID", "structopencoap__vars__t.html#a48b8857a8afbd63a6aa95cea074e9ca5", null ],
-    [ "resources", "structopencoap__vars__t.html#a8904a093f5c6302b8a1ea17d144fa1c3", null ]
-];
