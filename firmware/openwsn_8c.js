a683ded17bf37599b2423ea9c9f6a8a65d7ffd96
OpenWSN Builder <builder@openwsn.org>
2014-07-17 04:46:45 -0700

    removing old firmware documentation

--- firmware/openwsn_8c.js
+++
@@ -1,4 +0,0 @@
-var openwsn_8c =
-[
-    [ "openwsn_init", "openwsn_8c.html#aac95c43f45a25ab48d3eccda2d276509", null ]
-];
--- firmware/topology_8h.js
+++
@@ -1,4 +0,0 @@
-var topology_8h =
-[
-    [ "topology_isAcceptablePacket", "topology_8h.html#gab6eb1cb0c830e57a60ccf437d10e3e5b", null ]
-];
