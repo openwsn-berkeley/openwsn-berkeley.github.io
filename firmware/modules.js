126d5ae2a08d466995e8c76adca3c4dbfbe94366
OpenWSN Builder <builder@openwsn.org>
2014-07-18 04:46:58 -0700

    removing old firmware documentation

--- firmware/modules.js
+++
@@ -1,9 +0,0 @@
-var modules =
-[
-    [ "Drivers", "group__drivers.html", "group__drivers" ],
-    [ "Kernel", "group__kernel.html", "group__kernel" ],
-    [ "BSP", "group___b_s_p.html", "group___b_s_p" ],
-    [ "RES", "group___r_e_s.html", null ],
-    [ "Applications", "group___app.html", "group___app" ],
-    [ "OpenStack", "group__openstack.html", "group__openstack" ]
-];
