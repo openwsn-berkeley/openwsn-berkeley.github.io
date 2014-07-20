e6c86e3708e202823a23fe224af95ecc76c36182
OpenWSN Builder <builder@openwsn.org>
2014-07-20 04:46:35 -0700

    removing old firmware documentation

--- firmware/dir_d21dcc8e5cd226050080300fedb169e8.js
+++
@@ -1,5 +0,0 @@
-var dir_d21dcc8e5cd226050080300fedb169e8 =
-[
-    [ "udpinject.c", "udpinject_8c.html", "udpinject_8c" ],
-    [ "udpinject.h", "udpinject_8h.html", "udpinject_8h" ]
-];
--- firmware/structopentimers__vars__t.js
+++
@@ -1,6 +0,0 @@
-var structopentimers__vars__t =
-[
-    [ "currentTimeout", "structopentimers__vars__t.html#a9d3fd418f416bee724aaee3784fd2810", null ],
-    [ "running", "structopentimers__vars__t.html#a36f7b6be7108281af77939ceaec42fd6", null ],
-    [ "timersBuf", "structopentimers__vars__t.html#aa796d78937b8f891f173641d8752ad3b", null ]
-];
