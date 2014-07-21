d41afcae0606ce03a30c5a7ba9342104c95b4dbb
OpenWSN Builder <builder@openwsn.org>
2014-07-21 04:47:01 -0700

    adding new firmware documentation

---
+++ firmware/group___packet_functions.js
@@ -0,0 +1,24 @@
+var group___packet_functions =
+[
+    [ "packetfunctions_calculateChecksum", "group___packet_functions.html#ga35d4105117b00f89a87823ff34834ad9", null ],
+    [ "packetfunctions_calculateCRC", "group___packet_functions.html#ga16f1295774f4a73bd950ea958524884c", null ],
+    [ "packetfunctions_checkCRC", "group___packet_functions.html#ga71efba46301d8ac6a3236e90b4cf0c94", null ],
+    [ "packetfunctions_htonl", "group___packet_functions.html#gadab5be4bd6eea7fddd27f533fd5cb205", null ],
+    [ "packetfunctions_htons", "group___packet_functions.html#ga3e26888db2ba6e41c9eb6675dfa9c5f9", null ],
+    [ "packetfunctions_ip128bToMac64b", "group___packet_functions.html#ga3f23f27556fb610f16c170378eeabeec", null ],
+    [ "packetfunctions_isAllHostsMulticast", "group___packet_functions.html#ga4002d21fb26813d4652268eccf7e4acd", null ],
+    [ "packetfunctions_isAllRoutersMulticast", "group___packet_functions.html#ga47c60182596145c31db05b11926cdcb2", null ],
+    [ "packetfunctions_isBroadcastMulticast", "group___packet_functions.html#gaba1f21e2310fa6bb90ee23eba583e4de", null ],
+    [ "packetfunctions_mac16bToMac64b", "group___packet_functions.html#gab6e0f97c424d57d22794d2c4e0193990", null ],
+    [ "packetfunctions_mac64bToIp128b", "group___packet_functions.html#ga6d7fd056d46deba15d06d3dc86d3569a", null ],
+    [ "packetfunctions_mac64bToMac16b", "group___packet_functions.html#ga26987c269ac582016eaf905cec7b3ead", null ],
+    [ "packetfunctions_ntohl", "group___packet_functions.html#ga8525e8460e9cc8481008f56183d14d69", null ],
+    [ "packetfunctions_ntohs", "group___packet_functions.html#gab960a8cb0d85a7b628fbaf47d0887cad", null ],
+    [ "packetfunctions_readAddress", "group___packet_functions.html#ga8d69558563daaaa7d6829353b919cf91", null ],
+    [ "packetfunctions_reserveFooterSize", "group___packet_functions.html#gae3cb67c149428f8b5613cacbfa1a6e24", null ],
+    [ "packetfunctions_reserveHeaderSize", "group___packet_functions.html#ga690ad87588077409f382584732259925", null ],
+    [ "packetfunctions_sameAddress", "group___packet_functions.html#ga1f1461cad2a1e59c303d24fa8e165569", null ],
+    [ "packetfunctions_tossFooter", "group___packet_functions.html#ga54293366b7be24efa7bb2ebc73dcee23", null ],
+    [ "packetfunctions_tossHeader", "group___packet_functions.html#ga28450dafe3516a0b2275e564ef01fafb", null ],
+    [ "packetfunctions_writeAddress", "group___packet_functions.html#ga000bbaa2e29486905064fc77ef9f9653", null ]
+];
---
+++ firmware/group___t_o_p.js
@@ -0,0 +1,46 @@
+var group___t_o_p =
+[
+    [ "sixtop_vars_t", "structsixtop__vars__t.html", [
+      [ "busySendingAdv", "structsixtop__vars__t.html#a83792848649a87c7d1e0709441f586ac", null ],
+      [ "busySendingKa", "structsixtop__vars__t.html#a67dfc199837a5da81312d4a333e407c3", null ],
+      [ "commandID", "structsixtop__vars__t.html#ac0f6723a1fb1d7f47c5a11566b20dc67", null ],
+      [ "dsn", "structsixtop__vars__t.html#a3ec4442576203eb4b437528800ea9c61", null ],
+      [ "kaPeriod", "structsixtop__vars__t.html#a5f92fcc7db68362f22e28f02bd2c74a7", null ],
+      [ "MacMgtTaskCounter", "structsixtop__vars__t.html#a1deb1ec59cc045bf9ad8da815ed3f5e8", null ],
+      [ "periodMaintenance", "structsixtop__vars__t.html#a5e18aaac0e4a63ec1e33cbc42d342a03", null ],
+      [ "State", "structsixtop__vars__t.html#a8b7cbcda658086cb32359adbea6d0909", null ],
+      [ "timerId", "structsixtop__vars__t.html#a1952925d51e375d4da096207f03a8777", null ],
+      [ "TOtimerId", "structsixtop__vars__t.html#aa77b1f2a3d2c081341c804943b815de8", null ]
+    ] ],
+    [ "NO_UPPER_LAYER_CALLING_SIXTOP", "group___t_o_p.html#ga8fd9f4e311d76ba8d34c80a7e69a7371", null ],
+    [ "SIXTOP_PERIOD", "group___t_o_p.html#gae6ef7fac12df5e5e5319b673fb77b2a6", null ],
+    [ "sixtop_CommandID_num", "group___t_o_p.html#ga8161d14ed7c0b1839e3feb69349e8eb4", [
+      [ "SIXTOP_SOFT_CELL_REQ", "group___t_o_p.html#gga8161d14ed7c0b1839e3feb69349e8eb4a0ad40c703c2a81b01e3f1be8bea20ad5", null ],
+      [ "SIXTOP_SOFT_CELL_RESPONSE", "group___t_o_p.html#gga8161d14ed7c0b1839e3feb69349e8eb4a5da1b52bd5a45f8e74177273bc68713c", null ],
+      [ "SIXTOP_REMOVE_SOFT_CELL_REQUEST", "group___t_o_p.html#gga8161d14ed7c0b1839e3feb69349e8eb4a85a4d9f653487b2e5c0d61d4f99ccdd1", null ],
+      [ "SIXTOP_HARD_CELL_REQ", "group___t_o_p.html#gga8161d14ed7c0b1839e3feb69349e8eb4a30b21131f7a5fed7c004dfaf51e417cf", null ],
+      [ "SIXTOP_REMOVE_HARD_CELL", "group___t_o_p.html#gga8161d14ed7c0b1839e3feb69349e8eb4a816e44da1527907942b0a2c5a627b905", null ]
+    ] ],
+    [ "sixtop_state_t", "group___t_o_p.html#gac165fd3130d6e81664fe98717781e8b2", [
+      [ "S_IDLE", "group___t_o_p.html#ggac165fd3130d6e81664fe98717781e8b2a86e3fc319fdf083afe88308950abab44", null ],
+      [ "S_SIXTOP_LINKREQUEST_SEND", "group___t_o_p.html#ggac165fd3130d6e81664fe98717781e8b2ac005162eff118d0917e0cbfce174b79b", null ],
+      [ "S_WAIT_SIXTOP_LINKREQUEST_SENDDONE", "group___t_o_p.html#ggac165fd3130d6e81664fe98717781e8b2a0c0262ef6aa40abe7ab87caa37ff774e", null ],
+      [ "S_SIXTOP_LINKREQUEST_RECEIVE", "group___t_o_p.html#ggac165fd3130d6e81664fe98717781e8b2a1dd7820918a8117d67b11a05fdb49fbd", null ],
+      [ "S_WAIT_FORRESPONSE", "group___t_o_p.html#ggac165fd3130d6e81664fe98717781e8b2af767f7cb4bcc3fb259e1d586f4ea6efd", null ],
+      [ "S_SIXTOP_LINKRESPONSE_SEND", "group___t_o_p.html#ggac165fd3130d6e81664fe98717781e8b2a9566fcbeef5f148dae0c924117634d1c", null ],
+      [ "S_WAIT_SIXTOP_LINKRESPONSE_SENDDONE", "group___t_o_p.html#ggac165fd3130d6e81664fe98717781e8b2a5882344e598cc9bd403bca369a6c6895", null ],
+      [ "S_SIXTOP_LINKRESPONSE_RECEIVE", "group___t_o_p.html#ggac165fd3130d6e81664fe98717781e8b2a75363713a1f707a40f737685a0c2151c", null ],
+      [ "S_REMOVELINKREQUEST_SEND", "group___t_o_p.html#ggac165fd3130d6e81664fe98717781e8b2a0cba3a4fc67c48f9deb2bdc7a7141369", null ],
+      [ "S_WAIT_REMOVELINKREQUEST_SENDDONE", "group___t_o_p.html#ggac165fd3130d6e81664fe98717781e8b2a46a91727f954a7686bff9045691ca788", null ],
+      [ "S_REMOVELINKREQUEST_RECEIVE", "group___t_o_p.html#ggac165fd3130d6e81664fe98717781e8b2a4630f8ac5ff22b3e9945644a32d991de", null ]
+    ] ],
+    [ "debugPrint_myDAGrank", "group___t_o_p.html#ga5b5d77a858a5ac83abdf678d38d059e0", null ],
+    [ "sixtop_init", "group___t_o_p.html#ga7ebc63dd17f632acdb878a79c1d79da8", null ],
+    [ "sixtop_linkRequest", "group___t_o_p.html#ga47ac3d62a8c6ef77d99355b9f6bf9fa6", null ],
+    [ "sixtop_removeLinkRequest", "group___t_o_p.html#ga001043c783c13e25667d3c3db40d7500", null ],
+    [ "sixtop_send", "group___t_o_p.html#ga7e16122ce4b265f68df057c0fd852ecd", null ],
+    [ "sixtop_sendDone", "group___t_o_p.html#ga8fcd71c42553cdf10eb1d7f24c32f2be", null ],
+    [ "sixtop_setKaPeriod", "group___t_o_p.html#ga462512c8c301081df10b0d51f7e7bc8b", null ],
+    [ "task_sixtopNotifReceive", "group___t_o_p.html#ga04eb8f63cc93551e15143a21a6e6403e", null ],
+    [ "task_sixtopNotifSendDone", "group___t_o_p.html#ga7c754289c0f435249b69965524ad9368", null ]
+];
