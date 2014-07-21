107cd80744e1f2143151551b077ab332b6dcf588
OpenWSN Builder <builder@openwsn.org>
2014-07-21 04:46:46 -0700

    removing old firmware documentation

--- firmware/group__r6t.js
+++
@@ -1,7 +0,0 @@
-var group__r6t =
-[
-    [ "r6t_vars_t", "structr6t__vars__t.html", [
-      [ "desc", "structr6t__vars__t.html#af824c41339fef8ae82ca38c2ddc4eab8", null ]
-    ] ],
-    [ "r6t_init", "group__r6t.html#ga014edd7aedf5bf0e426740e28da28ba9", null ]
-];
--- firmware/structrsvp__label__request__t.js
+++
@@ -1,6 +0,0 @@
-var structrsvp__label__request__t =
-[
-    [ "header", "structrsvp__label__request__t.html#a24698cfa5f406e4f5e7d1df203cb989c", null ],
-    [ "lsp_enc_type", "structrsvp__label__request__t.html#a533383674a7f8c98017367531155ccc3", null ],
-    [ "lsp_sw_type", "structrsvp__label__request__t.html#ad4e7906a4c72e36d84c01484637c342c", null ]
-];
