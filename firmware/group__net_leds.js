8df0fe022549d3a00893f309ae13c112384d21c4
OpenWSN Builder <builder@openwsn.org>
2014-07-24 04:46:57 -0700

    removing old firmware documentation

--- firmware/group__net_leds.js
+++
@@ -1,7 +0,0 @@
-var group__net_leds =
-[
-    [ "rleds_vars_t", "structrleds__vars__t.html", [
-      [ "desc", "structrleds__vars__t.html#af824c41339fef8ae82ca38c2ddc4eab8", null ]
-    ] ],
-    [ "rleds__init", "group__net_leds.html#gaeff5d638d3ff076402569577d1c0666f", null ]
-];
