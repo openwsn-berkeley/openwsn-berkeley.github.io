36f134e50cc54f178d9937590a9698e93190b2c3
OpenWSN Builder <builder@openwsn.org>
2014-07-26 04:47:02 -0700

    removing old firmware documentation

--- firmware/group___app_tcp.js
+++
@@ -1,7 +0,0 @@
-var group___app_tcp =
-[
-    [ "ohlone", "group__ohlone.html", "group__ohlone" ],
-    [ "tcpEcho", "group__tcp_echo.html", "group__tcp_echo" ],
-    [ "tcpInject", "group__tcp_inject.html", "group__tcp_inject" ],
-    [ "tcpPrint", "group__tcp_print.html", "group__tcp_print" ]
-];
