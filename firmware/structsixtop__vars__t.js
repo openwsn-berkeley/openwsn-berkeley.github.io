3b4959ce5f4940cd1dcd774ff3c29cefaf1d74d8
OpenWSN Builder <builder@openwsn.org>
2014-07-28 04:48:29 -0700

    adding new firmware documentation

---
+++ firmware/structsixtop__vars__t.js
@@ -0,0 +1,13 @@
+var structsixtop__vars__t =
+[
+    [ "busySendingEB", "structsixtop__vars__t.html#aaa537d1a9cde3f60c07378e2d8aed666", null ],
+    [ "busySendingKA", "structsixtop__vars__t.html#ad39d52b224cb36783a3287f59796ff6c", null ],
+    [ "commandID", "structsixtop__vars__t.html#ac0f6723a1fb1d7f47c5a11566b20dc67", null ],
+    [ "dsn", "structsixtop__vars__t.html#a3ec4442576203eb4b437528800ea9c61", null ],
+    [ "kaPeriod", "structsixtop__vars__t.html#a5f92fcc7db68362f22e28f02bd2c74a7", null ],
+    [ "maintenanceTimerId", "structsixtop__vars__t.html#a81d509ed8bf4fdde83586c437878ba90", null ],
+    [ "mgtTaskCounter", "structsixtop__vars__t.html#a310130424da46be15f16c96653e0f1f2", null ],
+    [ "periodMaintenance", "structsixtop__vars__t.html#a5e18aaac0e4a63ec1e33cbc42d342a03", null ],
+    [ "six2six_state", "structsixtop__vars__t.html#a79bcd9a42ea4584bac814447d5d8cc39", null ],
+    [ "timeoutTimerId", "structsixtop__vars__t.html#abf3aca62b09c0270c0ac22e77eeb3594", null ]
+];
