6724d01568137713e82beee74df57485c3bc7413
OpenWSN Builder <builder@openwsn.org>
2014-07-29 04:49:40 -0700

    removing old firmware documentation

--- firmware/udpecho_8h.js
+++
@@ -1,7 +0,0 @@
-var udpecho_8h =
-[
-    [ "udpecho_debugPrint", "udpecho_8h.html#ga149b09f9314a5b834ec2bb72960a0f42", null ],
-    [ "udpecho_init", "udpecho_8h.html#gab288cc2f0b684d8cffe7f074724127e2", null ],
-    [ "udpecho_receive", "udpecho_8h.html#gaae93c1ba31874ed74ea285547836030e", null ],
-    [ "udpecho_sendDone", "udpecho_8h.html#gafa7d9fc3e6fa8562b76321dc79962513", null ]
-];
--- firmware/udpinject_8h.js
+++
@@ -1,8 +0,0 @@
-var udpinject_8h =
-[
-    [ "udpinject_debugPrint", "udpinject_8h.html#ga6a879da72ade78284e4fc497e6de67f2", null ],
-    [ "udpinject_init", "udpinject_8h.html#gaf175a27371235df259e18fcc9e577131", null ],
-    [ "udpinject_receive", "udpinject_8h.html#ga6409f050785dde9eba078f3f66d82c85", null ],
-    [ "udpinject_sendDone", "udpinject_8h.html#ga256547a8be06817e7c69c3306e380ead", null ],
-    [ "udpinject_trigger", "udpinject_8h.html#ga5c7663212895908339930b129651ca4d", null ]
-];
