b458e210eae5b03d66cb349ff3a635b57156964d
OpenWSN Builder <builder@openwsn.org>
2014-07-30 04:57:15 -0700

    removing old firmware documentation

--- firmware/struct_m_l_m_e___i_e__sub_header__t.js
+++
@@ -1,4 +0,0 @@
-var struct_m_l_m_e___i_e__sub_header__t =
-[
-    [ "length_subID_type", "struct_m_l_m_e___i_e__sub_header__t.html#a6bc4ab11603d8b2b8e5c7598c0318c22", null ]
-];
--- firmware/structicmpv6rpl__dao__target__ht.js
+++
@@ -1,7 +0,0 @@
-var structicmpv6rpl__dao__target__ht =
-[
-    [ "flags", "structicmpv6rpl__dao__target__ht.html#aa2585d779da0ab21273a8d92de9a0ebe", null ],
-    [ "optionLength", "structicmpv6rpl__dao__target__ht.html#accd37aae5350e1d88f68a6a43b1ca553", null ],
-    [ "prefixLength", "structicmpv6rpl__dao__target__ht.html#a80290f1f994532c1c3d0515de68f9e43", null ],
-    [ "type", "structicmpv6rpl__dao__target__ht.html#a1d127017fb298b889f4ba24752d08b8e", null ]
-];
