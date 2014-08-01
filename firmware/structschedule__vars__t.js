1f0b7bbc5a27cca2c38d98fcc3b23808b73b2af9
OpenWSN Builder <builder@openwsn.org>
2014-08-01 05:00:20 -0700

    adding new firmware documentation

---
+++ firmware/structschedule__vars__t.js
@@ -0,0 +1,9 @@
+var structschedule__vars__t =
+[
+    [ "backoff", "structschedule__vars__t.html#a8d745544f7ef05d1c33ddf538c20f326", null ],
+    [ "backoffExponent", "structschedule__vars__t.html#a492b6e376523ff79a2d6aeaff5bc2d38", null ],
+    [ "currentScheduleEntry", "structschedule__vars__t.html#a79a2bc38fe7d7754896ce53f8ad2a900", null ],
+    [ "debugPrintRow", "structschedule__vars__t.html#a09c19b543b338bf0f7146ebb33caddb4", null ],
+    [ "frameLength", "structschedule__vars__t.html#a913c707afe608546cd7af8e0670795fb", null ],
+    [ "scheduleBuf", "structschedule__vars__t.html#a30f51f31243d2d50890c66ced9c94367", null ]
+];
