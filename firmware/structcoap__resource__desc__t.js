37b9fc29547cd9e2ea6515e86529328468d886bc
OpenWSN Builder <builder@openwsn.org>
2014-08-06 04:47:10 -0700

    adding new firmware documentation

---
+++ firmware/structcoap__resource__desc__t.js
@@ -0,0 +1,13 @@
+var structcoap__resource__desc__t =
+[
+    [ "callbackRx", "structcoap__resource__desc__t.html#aeaa33d6840cfc1778fa2bdbdca204d8c", null ],
+    [ "callbackSendDone", "structcoap__resource__desc__t.html#a4d2a75d06295789331f439871f4190f3", null ],
+    [ "componentID", "structcoap__resource__desc__t.html#a4fdca17720603d2904382a8090fcd306", null ],
+    [ "messageID", "structcoap__resource__desc__t.html#a48b8857a8afbd63a6aa95cea074e9ca5", null ],
+    [ "next", "structcoap__resource__desc__t.html#a071de1f4b7cf41aa93e7d5c3cff98868", null ],
+    [ "path0len", "structcoap__resource__desc__t.html#af9a6e939da8557265cf0554bfee567bc", null ],
+    [ "path0val", "structcoap__resource__desc__t.html#a0fab9d3155ca9b3660d9566454c82b81", null ],
+    [ "path1len", "structcoap__resource__desc__t.html#ab5b1bde7b55b933140277d3726f83524", null ],
+    [ "path1val", "structcoap__resource__desc__t.html#a45769cd01822173f089b6c44f7035676", null ],
+    [ "token", "structcoap__resource__desc__t.html#afb6cdb8d697f6e4b925161d99a2daa3c", null ]
+];
---
+++ firmware/structicmpv6echo__vars__t.js
@@ -0,0 +1,6 @@
+var structicmpv6echo__vars__t =
+[
+    [ "busySending", "structicmpv6echo__vars__t.html#adb63c747385c70bc79674b73f2a06dcc", null ],
+    [ "hisAddress", "structicmpv6echo__vars__t.html#a5878b2b3978e0ceee14715db652dd5ad", null ],
+    [ "seq", "structicmpv6echo__vars__t.html#a0a97914b75ed3ab7d0e13d8b200d758a", null ]
+];
