d474cd767ff08022269a10c4ab48a52155aff336
OpenWSN Builder <builder@openwsn.org>
2014-08-07 04:46:28 -0700

    adding new firmware documentation

---
+++ firmware/group___open_bridge.js
@@ -0,0 +1,7 @@
+var group___open_bridge =
+[
+    [ "openbridge_init", "group___open_bridge.html#ga34778c5d5b048f0db1b588bd88f3fb47", null ],
+    [ "openbridge_receive", "group___open_bridge.html#ga59de967f7924e1c672ada8905a54d683", null ],
+    [ "openbridge_sendDone", "group___open_bridge.html#ga48345d223f59957ed3b4b47ed692ef98", null ],
+    [ "openbridge_triggerData", "group___open_bridge.html#gaa9c3e5722db7e8c313d5d36c8269203e", null ]
+];
