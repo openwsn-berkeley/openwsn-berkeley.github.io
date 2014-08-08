c05b4f706bcf58ececf00028625948eba7525bf7
OpenWSN Builder <builder@openwsn.org>
2014-08-08 04:46:34 -0700

    removing old firmware documentation

--- firmware/group__tcp_print.js
+++
@@ -1,9 +0,0 @@
-var group__tcp_print =
-[
-    [ "tcpprint_connectDone", "group__tcp_print.html#ga3ae4601ab6a352f9a2bc3abc26db0aeb", null ],
-    [ "tcpprint_debugPrint", "group__tcp_print.html#ga6e8d33476751a7a2188abd9a7ae63bd8", null ],
-    [ "tcpprint_init", "group__tcp_print.html#gac6cac80d307211a3b43e372d2b1fe587", null ],
-    [ "tcpprint_receive", "group__tcp_print.html#ga55cf0f6800d5e7c3334715965daa3dfd", null ],
-    [ "tcpprint_sendDone", "group__tcp_print.html#ga0ce5203f43279ec1caf7cc65182e6e4c", null ],
-    [ "tcpprint_shouldIlisten", "group__tcp_print.html#gaf8f7628a93525d43b1eec832b3babb2a", null ]
-];
--- firmware/structcycle__table__tuple__t.js
+++
@@ -1,5 +0,0 @@
-var structcycle__table__tuple__t =
-[
-    [ "num_cycles", "structcycle__table__tuple__t.html#aa3f31c23a221cbb2f9df381c11d77ee5", null ],
-    [ "num_participants", "structcycle__table__tuple__t.html#a13f61ad666040f26d37b93021d0c3223", null ]
-];
