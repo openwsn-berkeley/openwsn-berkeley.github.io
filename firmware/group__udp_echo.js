5da5072c8fb9ca2fa7d547d8d013db8989772d3e
OpenWSN Builder <builder@openwsn.org>
2014-08-10 04:47:06 -0700

    removing old firmware documentation

--- firmware/group__udp_echo.js
+++
@@ -1,7 +0,0 @@
-var group__udp_echo =
-[
-    [ "udpecho_debugPrint", "group__udp_echo.html#ga149b09f9314a5b834ec2bb72960a0f42", null ],
-    [ "udpecho_init", "group__udp_echo.html#gab288cc2f0b684d8cffe7f074724127e2", null ],
-    [ "udpecho_receive", "group__udp_echo.html#gaae93c1ba31874ed74ea285547836030e", null ],
-    [ "udpecho_sendDone", "group__udp_echo.html#gafa7d9fc3e6fa8562b76321dc79962513", null ]
-];
