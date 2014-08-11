db0ddbc8dfe613722329f99ceeb5b0ad3022c76c
OpenWSN Builder <builder@openwsn.org>
2014-08-11 04:46:45 -0700

    removing old firmware documentation

--- firmware/tcpprint_8c.js
+++
@@ -1,9 +0,0 @@
-var tcpprint_8c =
-[
-    [ "tcpprint_connectDone", "tcpprint_8c.html#ga3ae4601ab6a352f9a2bc3abc26db0aeb", null ],
-    [ "tcpprint_debugPrint", "tcpprint_8c.html#ga6e8d33476751a7a2188abd9a7ae63bd8", null ],
-    [ "tcpprint_init", "tcpprint_8c.html#gac6cac80d307211a3b43e372d2b1fe587", null ],
-    [ "tcpprint_receive", "tcpprint_8c.html#ga55cf0f6800d5e7c3334715965daa3dfd", null ],
-    [ "tcpprint_sendDone", "tcpprint_8c.html#ga0ce5203f43279ec1caf7cc65182e6e4c", null ],
-    [ "tcpprint_shouldIlisten", "tcpprint_8c.html#gaf8f7628a93525d43b1eec832b3babb2a", null ]
-];
