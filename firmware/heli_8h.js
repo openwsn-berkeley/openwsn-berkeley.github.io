50f8b937e6d27373c6a84088bb83cb863d108c0e
OpenWSN Builder <builder@openwsn.org>
2014-08-13 04:47:25 -0700

    removing old firmware documentation

--- firmware/heli_8h.js
+++
@@ -1,8 +0,0 @@
-var heli_8h =
-[
-    [ "heli_debugPrint", "heli_8h.html#gad4bac7d01ad7f689787847ce201969aa", null ],
-    [ "heli_init", "heli_8h.html#gaa0ae5ec500247e320545e1e427e56519", null ],
-    [ "heli_receive", "heli_8h.html#ga6dc72ff728d4cf2009314f7e9f984cd5", null ],
-    [ "heli_sendDone", "heli_8h.html#ga31dc3e7a2d86139164b443788901338f", null ],
-    [ "heli_trigger", "heli_8h.html#ga6308a9ac5bd4eb2e5998785a3a58c735", null ]
-];
--- firmware/otf_8c.js
+++
@@ -1,8 +0,0 @@
-var otf_8c =
-[
-    [ "otf_addCell_task", "otf_8c.html#a28566748a9e73a0e87f67369cbe8ecba", null ],
-    [ "otf_init", "otf_8c.html#ga04b7c81141e2ab79d1b8744eb330a965", null ],
-    [ "otf_notif_addedCell", "otf_8c.html#ga4e965d3109977c730242358ca6429c6e", null ],
-    [ "otf_notif_removedCell", "otf_8c.html#ga82922e93857a8b5b6f7fb5a84053abf3", null ],
-    [ "otf_removeCell_task", "otf_8c.html#a0e9b14a8f371bbdb5482450575b00020", null ]
-];
