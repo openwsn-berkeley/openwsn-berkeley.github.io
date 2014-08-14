878990b8375ac68434196ee14d94a3e2c102663b
OpenWSN Builder <builder@openwsn.org>
2014-08-14 04:49:20 -0700

    removing old firmware documentation

--- firmware/otf_8h.js
+++
@@ -1,6 +0,0 @@
-var otf_8h =
-[
-    [ "otf_init", "otf_8h.html#ga04b7c81141e2ab79d1b8744eb330a965", null ],
-    [ "otf_notif_addedCell", "otf_8h.html#ga4e965d3109977c730242358ca6429c6e", null ],
-    [ "otf_notif_removedCell", "otf_8h.html#ga82922e93857a8b5b6f7fb5a84053abf3", null ]
-];
--- firmware/rwellknown_8h.js
+++
@@ -1,4 +0,0 @@
-var rwellknown_8h =
-[
-    [ "rwellknown_init", "rwellknown_8h.html#ga30d2769993881f1f58a70c750db8b09f", null ]
-];
