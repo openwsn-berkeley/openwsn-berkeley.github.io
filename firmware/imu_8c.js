f5e2123c677b46af5f1ac6810a3ce944cb5254dd
OpenWSN Builder <builder@openwsn.org>
2014-08-16 04:47:42 -0700

    adding new firmware documentation

---
+++ firmware/imu_8c.js
@@ -0,0 +1,12 @@
+var imu_8c =
+[
+    [ "imu_vars_t", "structimu__vars__t.html", "structimu__vars__t" ],
+    [ "imu_debugPrint", "imu_8c.html#ga27a020212451353e8d09f34e74868035", null ],
+    [ "imu_init", "imu_8c.html#ga86a06fe7a9831292bd92867da2d12f80", null ],
+    [ "imu_receive", "imu_8c.html#ga4c059ec6a973818cc9fec567243a479a", null ],
+    [ "imu_reset", "imu_8c.html#ac6a5de026fbe8e2151977b9eeffa4fe7", null ],
+    [ "imu_send", "imu_8c.html#ae98f692f4acd7bceae2be28a7b1ef703", null ],
+    [ "imu_sendDone", "imu_8c.html#ga649d143ef86aedba318bf5a73eb62466", null ],
+    [ "imu_trigger", "imu_8c.html#ga56af1173e958fbb4565e2a9523a77a3b", null ],
+    [ "imu_vars", "imu_8c.html#a069796ae9b075510630e66eea42de4fc", null ]
+];
