9afe60d0577ea62547b1aa106e1b3af1aa00331c
OpenWSN Builder <builder@openwsn.org>
2014-08-17 04:47:44 -0700

    removing old firmware documentation

--- firmware/group__eui64.js
+++
@@ -1,4 +0,0 @@
-var group__eui64 =
-[
-    [ "eui64_get", "group__eui64.html#gae49dc9068e3f46bfd188579192fc036b", null ]
-];
--- firmware/structcell_info__ht.js
+++
@@ -1,6 +0,0 @@
-var structcell_info__ht =
-[
-    [ "choffset", "structcell_info__ht.html#ad9b28672a758510f41c39b6a07f92117", null ],
-    [ "linkoptions", "structcell_info__ht.html#aaf0b0887ef9a6e99c7d883b99f522b7d", null ],
-    [ "tsNum", "structcell_info__ht.html#adb779a5c8f8d3aff0c83b0ece95394a1", null ]
-];
