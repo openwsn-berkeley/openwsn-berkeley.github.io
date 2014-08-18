9fe52047a876958fe7bf0a16d94f560348adc922
OpenWSN Builder <builder@openwsn.org>
2014-08-18 04:47:56 -0700

    removing old firmware documentation

--- firmware/rsvp_8c.js
+++
@@ -1,8 +0,0 @@
-var rsvp_8c =
-[
-    [ "rsvp_vars_t", "structrsvp__vars__t.html", "structrsvp__vars__t" ],
-    [ "rsvp_init", "rsvp_8c.html#aed99b6fd1bc7de1cbefc05d761a4adda", null ],
-    [ "rsvp_qos_request", "rsvp_8c.html#a538457baec6521cecc295947d24009e7", null ],
-    [ "rsvp_timer_cb", "rsvp_8c.html#a8223b1b5ca1e9d5fc32c4f25cbb7ce37", null ],
-    [ "rsvp_vars", "rsvp_8c.html#aa389fdcd40ef7ce58a139d40313ee7f9", null ]
-];
