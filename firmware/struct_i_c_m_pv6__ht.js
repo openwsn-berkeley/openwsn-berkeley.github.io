3d145548d08156459cd123b945619b5279a75d54
OpenWSN Builder <builder@openwsn.org>
2014-08-19 04:47:46 -0700

    removing old firmware documentation

--- firmware/struct_i_c_m_pv6__ht.js
+++
@@ -1,6 +0,0 @@
-var struct_i_c_m_pv6__ht =
-[
-    [ "checksum", "struct_i_c_m_pv6__ht.html#a6796c847bbd16253ce2538754c250bd6", null ],
-    [ "code", "struct_i_c_m_pv6__ht.html#a966576744a473fafb7687f8e5649941f", null ],
-    [ "type", "struct_i_c_m_pv6__ht.html#a1d127017fb298b889f4ba24752d08b8e", null ]
-];
