9c6cf01112d78c20c6f6aa2b4baf0c71f15d4356
OpenWSN Builder <builder@openwsn.org>
2014-08-20 04:48:00 -0700

    adding new firmware documentation

---
+++ firmware/imu_8h.js
@@ -0,0 +1,8 @@
+var imu_8h =
+[
+    [ "imu_debugPrint", "imu_8h.html#ga27a020212451353e8d09f34e74868035", null ],
+    [ "imu_init", "imu_8h.html#ga86a06fe7a9831292bd92867da2d12f80", null ],
+    [ "imu_receive", "imu_8h.html#ga4c059ec6a973818cc9fec567243a479a", null ],
+    [ "imu_sendDone", "imu_8h.html#ga649d143ef86aedba318bf5a73eb62466", null ],
+    [ "imu_trigger", "imu_8h.html#ga56af1173e958fbb4565e2a9523a77a3b", null ]
+];
