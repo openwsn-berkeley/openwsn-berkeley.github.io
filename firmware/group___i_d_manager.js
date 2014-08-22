fd9de5d7186673d2092f110cb4fc7d4edf047e48
OpenWSN Builder <builder@openwsn.org>
2014-08-22 04:48:02 -0700

    adding new firmware documentation

---
+++ firmware/group___i_d_manager.js
@@ -0,0 +1,33 @@
+var group___i_d_manager =
+[
+    [ "debugIDManagerEntry_t", "structdebug_i_d_manager_entry__t.html", [
+      [ "isBridge", "structdebug_i_d_manager_entry__t.html#a018b91ffc2fd9cb7e2bab2790ca45d79", null ],
+      [ "isDAGroot", "structdebug_i_d_manager_entry__t.html#a0ad47ab43cefa963b2921da022e1658f", null ],
+      [ "my16bID", "structdebug_i_d_manager_entry__t.html#a3ddec815683ae0665f5919cd798cd3a8", null ],
+      [ "my64bID", "structdebug_i_d_manager_entry__t.html#a95fcc4bca1fd0b4f05fe13714bb65eb6", null ],
+      [ "myPANID", "structdebug_i_d_manager_entry__t.html#a6e395f63c8294c9147390ff717390af3", null ],
+      [ "myPrefix", "structdebug_i_d_manager_entry__t.html#a87338ea066a746cb0844ee367ed46c54", null ]
+    ] ],
+    [ "idmanager_vars_t", "structidmanager__vars__t.html", [
+      [ "isBridge", "structidmanager__vars__t.html#a018b91ffc2fd9cb7e2bab2790ca45d79", null ],
+      [ "isDAGroot", "structidmanager__vars__t.html#a0ad47ab43cefa963b2921da022e1658f", null ],
+      [ "my16bID", "structidmanager__vars__t.html#a3ddec815683ae0665f5919cd798cd3a8", null ],
+      [ "my64bID", "structidmanager__vars__t.html#a95fcc4bca1fd0b4f05fe13714bb65eb6", null ],
+      [ "myPANID", "structidmanager__vars__t.html#a6e395f63c8294c9147390ff717390af3", null ],
+      [ "myPrefix", "structidmanager__vars__t.html#a87338ea066a746cb0844ee367ed46c54", null ]
+    ] ],
+    [ "ACTION_NO", "group___i_d_manager.html#ga7a8c6698e6d3bf6fb3528ba07aa8d5eb", null ],
+    [ "ACTION_TOGGLE", "group___i_d_manager.html#ga935e9aef6a097c624a9b3b6147fbf197", null ],
+    [ "ACTION_YES", "group___i_d_manager.html#ga75a645b8c3a1fdda2c11bb1ec2a0f440", null ],
+    [ "debugPrint_id", "group___i_d_manager.html#gaa69fa284f9b777d2f5f600f94c3eb44b", null ],
+    [ "idmanager_getIsBridge", "group___i_d_manager.html#ga48ebb3103cc6d70bd0fb81ee3efdd133", null ],
+    [ "idmanager_getIsDAGroot", "group___i_d_manager.html#gaa792e6561333f1615bbc61392e826d39", null ],
+    [ "idmanager_getMyID", "group___i_d_manager.html#gac95b57a051d12bff7b5c3e3599ce4aa5", null ],
+    [ "idmanager_init", "group___i_d_manager.html#ga3e974443ae75269d00c3de03992a1669", null ],
+    [ "idmanager_isMyAddress", "group___i_d_manager.html#ga4cff67772250923086a3d6476eb673c8", null ],
+    [ "idmanager_setIsBridge", "group___i_d_manager.html#ga54ac6649a24946b461f0906d0f862dc6", null ],
+    [ "idmanager_setIsDAGroot", "group___i_d_manager.html#gab463c5b7e0b714691efe0c46629705f7", null ],
+    [ "idmanager_setMyID", "group___i_d_manager.html#ga5e2f6326c043a60f46e019aa538ca7d2", null ],
+    [ "idmanager_triggerAboutBridge", "group___i_d_manager.html#gaa3d82ebb32c2c1b03d06155ee4e8f7ed", null ],
+    [ "idmanager_triggerAboutRoot", "group___i_d_manager.html#ga464f5751bc371a97b41407697e6817a3", null ]
+];
---
+++ firmware/structrsvp__path__msg__t.js
@@ -0,0 +1,10 @@
+var structrsvp__path__msg__t =
+[
+    [ "header", "structrsvp__path__msg__t.html#a4fe5e56873d6e3f22efd2d5f39e5b7f0", null ],
+    [ "hop", "structrsvp__path__msg__t.html#ac4e69f3478e9158735b531a428b0922c", null ],
+    [ "label_request", "structrsvp__path__msg__t.html#a064e6fec293c2cf74eb85afd08c965be", null ],
+    [ "sender_llnspec", "structrsvp__path__msg__t.html#a27e9b35b36e3a3a19b3cdd6465cb9c26", null ],
+    [ "sender_template", "structrsvp__path__msg__t.html#a01f7b7347da1e47b0b7e005dbae74b46", null ],
+    [ "session", "structrsvp__path__msg__t.html#a76eab5d99589331d6b6f30a0d66e1746", null ],
+    [ "time_values", "structrsvp__path__msg__t.html#ace92676b4bd1e7cdcfec126a70e4fc90", null ]
+];
