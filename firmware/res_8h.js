7fa41d155c1bb0c1d00bde5606434dc305e9b68a
OpenWSN Builder <builder@openwsn.org>
2014-08-28 04:50:15 -0700

    removing old firmware documentation

--- firmware/res_8h.js
+++
@@ -1,9 +0,0 @@
-var res_8h =
-[
-    [ "debugPrint_myDAGrank", "res_8h.html#ga5b5d77a858a5ac83abdf678d38d059e0", null ],
-    [ "res_init", "res_8h.html#ga75ea1e7db757206720d32d981b38bd1b", null ],
-    [ "res_send", "res_8h.html#ga93268856a3d1782e7b29a2fdb04c0b85", null ],
-    [ "res_setKaPeriod", "res_8h.html#gaac12bdbf2fbffcb1fa3decd63bb10e94", null ],
-    [ "task_resNotifReceive", "res_8h.html#gaf83dab2e0b5de8f44eca2731c2e3f6c6", null ],
-    [ "task_resNotifSendDone", "res_8h.html#ga7a05e82c51617c34a50505085bcf9252", null ]
-];
--- firmware/structpayload___i_e__ht.js
+++
@@ -1,4 +0,0 @@
-var structpayload___i_e__ht =
-[
-    [ "length_groupid_type", "structpayload___i_e__ht.html#a91325f7dad3a5bfb1a3dda0c6334ed05", null ]
-];
