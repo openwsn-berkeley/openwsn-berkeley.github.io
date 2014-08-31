10ecbd0d17d49dac0cbf6651791f1597e91d0b7b
OpenWSN Builder <builder@openwsn.org>
2014-08-31 04:53:18 -0700

    removing old firmware documentation

--- firmware/structrsvp__hop__t.js
+++
@@ -1,6 +0,0 @@
-var structrsvp__hop__t =
-[
-    [ "address", "structrsvp__hop__t.html#ae32b304b1d81324031502236bce42e91", null ],
-    [ "header", "structrsvp__hop__t.html#a24698cfa5f406e4f5e7d1df203cb989c", null ],
-    [ "local_interface_handle", "structrsvp__hop__t.html#aae1ee3aabbfad956c094fe185acc6fe8", null ]
-];
