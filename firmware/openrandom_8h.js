8dc06d7584aab1d951b192cf00b5ae51f209adf3
OpenWSN Builder <builder@openwsn.org>
2014-09-01 05:03:26 -0700

    removing old firmware documentation

--- firmware/openrandom_8h.js
+++
@@ -1,5 +0,0 @@
-var openrandom_8h =
-[
-    [ "openrandom_get16b", "openrandom_8h.html#ga70626e5a23d14d7d426517fb5389252d", null ],
-    [ "openrandom_init", "openrandom_8h.html#gaba64bed43b28e866086767c9daf83390", null ]
-];
