182e7876ecc8cf96bd51f80146d09781b2bba697
OpenWSN Builder <builder@openwsn.org>
2014-09-12 04:48:23 -0700

    removing old firmware documentation

--- firmware/dir_4916502eeeee2174d69d9d347bc18007.js
+++
@@ -1,4 +0,0 @@
-var dir_4916502eeeee2174d69d9d347bc18007 =
-[
-    [ "openos", "dir_f8f7d70f6415baf543d11d3df711609b.html", "dir_f8f7d70f6415baf543d11d3df711609b" ]
-];
--- firmware/group__drivers.js
+++
@@ -1,6 +0,0 @@
-var group__drivers =
-[
-    [ "HDLC", "group___h_d_l_c.html", "group___h_d_l_c" ],
-    [ "OpenSerial", "group___open_serial.html", "group___open_serial" ],
-    [ "OpenTimers", "group___open_timers.html", "group___open_timers" ]
-];
