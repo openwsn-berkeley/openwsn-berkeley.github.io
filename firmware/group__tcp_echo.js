ab9449a363af774d1742b826e70b0c6c75fd16fa
OpenWSN Builder <builder@openwsn.org>
2014-09-16 04:46:36 -0700

    removing old firmware documentation

--- firmware/group__tcp_echo.js
+++
@@ -1,9 +0,0 @@
-var group__tcp_echo =
-[
-    [ "tcpecho_connectDone", "group__tcp_echo.html#gad9ef9f7304903148f45eb561f91d693a", null ],
-    [ "tcpecho_debugPrint", "group__tcp_echo.html#gadc98b997cb63084f91100b034b47ceb9", null ],
-    [ "tcpecho_init", "group__tcp_echo.html#gad50d0fe98332b382c0ee6650b2e426bf", null ],
-    [ "tcpecho_receive", "group__tcp_echo.html#ga8425b2d9ec62b9865ebe4416ed75267e", null ],
-    [ "tcpecho_sendDone", "group__tcp_echo.html#ga50116ff82b4afaab52388efcee6fc4f6", null ],
-    [ "tcpecho_shouldIlisten", "group__tcp_echo.html#gad1ac68d6580f567db69ad21077e0c45e", null ]
-];
--- firmware/tcpecho_8h.js
+++
@@ -1,9 +0,0 @@
-var tcpecho_8h =
-[
-    [ "tcpecho_connectDone", "tcpecho_8h.html#gad9ef9f7304903148f45eb561f91d693a", null ],
-    [ "tcpecho_debugPrint", "tcpecho_8h.html#gadc98b997cb63084f91100b034b47ceb9", null ],
-    [ "tcpecho_init", "tcpecho_8h.html#gad50d0fe98332b382c0ee6650b2e426bf", null ],
-    [ "tcpecho_receive", "tcpecho_8h.html#ga8425b2d9ec62b9865ebe4416ed75267e", null ],
-    [ "tcpecho_sendDone", "tcpecho_8h.html#ga50116ff82b4afaab52388efcee6fc4f6", null ],
-    [ "tcpecho_shouldIlisten", "tcpecho_8h.html#gad1ac68d6580f567db69ad21077e0c45e", null ]
-];
