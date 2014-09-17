fed69f3f799de23370722c9903eb5c33c4993036
OpenWSN Builder <builder@openwsn.org>
2014-09-17 04:47:01 -0700

    removing old firmware documentation

--- firmware/dir_f8a3f50a4d32ecf31a66021985e88f64.js
+++
@@ -1,5 +0,0 @@
-var dir_f8a3f50a4d32ecf31a66021985e88f64 =
-[
-    [ "rrt.c", "rrt_8c.html", "rrt_8c" ],
-    [ "rrt.h", "rrt_8h.html", "rrt_8h" ]
-];
--- firmware/structudpstorm__vars__t.js
+++
@@ -1,6 +0,0 @@
-var structudpstorm__vars__t =
-[
-    [ "desc", "structudpstorm__vars__t.html#af824c41339fef8ae82ca38c2ddc4eab8", null ],
-    [ "period", "structudpstorm__vars__t.html#a258408d6d5d13a24bfa5211d81ce1682", null ],
-    [ "timerId", "structudpstorm__vars__t.html#a1952925d51e375d4da096207f03a8777", null ]
-];
