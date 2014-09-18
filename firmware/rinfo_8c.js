fcb0ae2ce0afb69b7b5676e17aaf2ac664144af7
OpenWSN Builder <builder@openwsn.org>
2014-09-18 04:46:53 -0700

    adding new firmware documentation

---
+++ firmware/rinfo_8c.js
@@ -0,0 +1,8 @@
+var rinfo_8c =
+[
+    [ "rinfo_init", "rinfo_8c.html#gaf7cf26c7f133967fc1cfe5c7c5168f85", null ],
+    [ "rinfo_receive", "rinfo_8c.html#a0bf2ed02d8b4cf2ecc06654f4019b757", null ],
+    [ "rinfo_sendDone", "rinfo_8c.html#a11f9cf989dcd8fb3d621a112e2c035b5", null ],
+    [ "rinfo_path0", "rinfo_8c.html#a39c759cea3b4e9e99f79d5687b34a913", null ],
+    [ "rinfo_vars", "rinfo_8c.html#ac79cb5413e66a4d5e8cc2ee674c8e1db", null ]
+];
