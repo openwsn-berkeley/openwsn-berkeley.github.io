49e6170513bf714f1842dd531e188b02856d62da
OpenWSN Builder <builder@openwsn.org>
2014-09-19 04:47:01 -0700

    removing old firmware documentation

--- firmware/gmpls_8c.js
+++
@@ -1,9 +0,0 @@
-var gmpls_8c =
-[
-    [ "mpls_vars_t", "structmpls__vars__t.html", "structmpls__vars__t" ],
-    [ "mpls_init", "gmpls_8c.html#a6ede47092128f29048a05acf51be29a7", null ],
-    [ "mpls_receive", "gmpls_8c.html#a0b54eecb39ddf1a1bd95c2926f33ecac", null ],
-    [ "mpls_send", "gmpls_8c.html#ae3cef9cec8745bbd469918877b65f5bf", null ],
-    [ "mpls_timer_cb", "gmpls_8c.html#a75b23dcbb14fb0dfaab966fdfbc4b664", null ],
-    [ "mpls_vars", "gmpls_8c.html#af7a3eae839f26d76e9751018e7b522af", null ]
-];
--- firmware/group__r_t.js
+++
@@ -1,6 +0,0 @@
-var group__r_t =
-[
-    [ "layerdebug_init", "group__r_t.html#ga39e268c02b9f0b865ed49d8ba00bf477", null ],
-    [ "rex_init", "group__r_t.html#ga21db794e5c1706828b6b5983c2f577cf", null ],
-    [ "rt_init", "group__r_t.html#gad0632adb7bb2c3f8acba9213fc50fdb8", null ]
-];
