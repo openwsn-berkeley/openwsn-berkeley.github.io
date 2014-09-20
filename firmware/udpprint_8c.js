91025d0cdd5cd643b3f53762afe7d94d29afb5cc
OpenWSN Builder <builder@openwsn.org>
2014-09-20 04:46:53 -0700

    removing old firmware documentation

--- firmware/udpprint_8c.js
+++
@@ -1,7 +0,0 @@
-var udpprint_8c =
-[
-    [ "udpprint_debugPrint", "udpprint_8c.html#gaf2c96d3fdec51ee4c182f16e91be7349", null ],
-    [ "udpprint_init", "udpprint_8c.html#ga51bc000cda59acdfba735f9968bca77c", null ],
-    [ "udpprint_receive", "udpprint_8c.html#gaa09538714cb8ad49b2349143d085081f", null ],
-    [ "udpprint_sendDone", "udpprint_8c.html#ga8d03e274ebc621537558336ec87987c5", null ]
-];
