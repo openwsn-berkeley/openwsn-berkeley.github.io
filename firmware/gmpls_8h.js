7966474c6a87678bed29f7061f9c3e2c173461c6
OpenWSN Builder <builder@openwsn.org>
2014-09-20 04:47:06 -0700

    adding new firmware documentation

---
+++ firmware/gmpls_8h.js
@@ -0,0 +1,6 @@
+var gmpls_8h =
+[
+    [ "mpls_init", "gmpls_8h.html#adeb9dfc118809cf4f736c483d5828955", null ],
+    [ "mpls_receive", "gmpls_8h.html#a0b54eecb39ddf1a1bd95c2926f33ecac", null ],
+    [ "mpls_send", "gmpls_8h.html#ae3cef9cec8745bbd469918877b65f5bf", null ]
+];
