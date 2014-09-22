49973d48633754e139ffa3336652d1bbe156c646
OpenWSN Builder <builder@openwsn.org>
2014-09-22 04:46:32 -0700

    removing old firmware documentation

--- firmware/rrt_8h.js
+++
@@ -1,4 +0,0 @@
-var rrt_8h =
-[
-    [ "rrt_init", "rrt_8h.html#ga4175f20cafc20669a11649e60e46455e", null ]
-];
--- firmware/structsync___i_e__ht.js
+++
@@ -1,5 +0,0 @@
-var structsync___i_e__ht =
-[
-    [ "asn", "structsync___i_e__ht.html#abdecca20a05b214c5fef4d57521a31fb", null ],
-    [ "join_priority", "structsync___i_e__ht.html#a2bd90b1232b9c642a3d6f9d9ee338d99", null ]
-];
