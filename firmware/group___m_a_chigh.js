d47514b0a1b3a9b0c365b506b20487c90b844aa5
OpenWSN Builder <builder@openwsn.org>
2014-09-23 04:47:04 -0700

    removing old firmware documentation

--- firmware/group___m_a_chigh.js
+++
@@ -1,7 +0,0 @@
-var group___m_a_chigh =
-[
-    [ "Neighbors", "group___neighbors.html", "group___neighbors" ],
-    [ "otf", "group__otf.html", "group__otf" ],
-    [ "Schedule", "group___schedule.html", "group___schedule" ],
-    [ "sixtop", "group__sixtop.html", "group__sixtop" ]
-];
